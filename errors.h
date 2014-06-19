ebcee601d0318cfb4250d9f67d31724c712df0c9
Steven Stewart <s26stewa@uwaterloo.ca>
2014-06-19 16:27:22 -0400

    File containing error codes for low-level error-handling.

---
+++ errors.h
@@ -0,0 +1,21 @@
+/* 
+ * File:        errors.h
+ * Author(s):   steve@map-d.com
+ *
+ * Created on June 19, 2014, 4:22 PM
+ */
+
+#ifndef _ERRORS_H
+#define	_ERRORS_H
+
+typedef enum _mapd_err_t {
+    MAPD_ERR =               0,     // generic error number
+    MAPD_ERR_FILE_OPEN =    -1,     // unable to open file
+    MAPD_ERR_FILE_CLOSE =   -2,     // error closing file
+    MAPD_ERR_FILE_WRITE =   -3,     // error writing file
+    MAPD_ERR_FILE_READ =    -4,     // error reading file
+    MAPD_ERR_FILE_APPEND =  -5      // error appending to file
+} mapd_err_t;
+
+#endif	/* ERRORS_H */
+
