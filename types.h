c01a5bad6773980c48c235f8e00e576c9bc46fa2
Steven Stewart <s26stewa@uwaterloo.ca>
2014-06-19 16:58:51 -0400

    This file contains some shared types needed by the FileMgr and File classes.

---
+++ types.h
@@ -0,0 +1,19 @@
+/* 
+ * File:        types.h
+ * Author(s):   steve@map-d.com
+ *
+ * Created on June 19, 2014, 4:29 PM
+ */
+
+#ifndef _TYPES_H
+#define	_TYPES_H
+
+// definition of a byte type (_byte_t) and a pointer to a byte type (*byte_t)
+typedef unsigned char _byte_t;
+typedef _byte_t *byte_t;
+
+// definition of Map-D size type
+typedef unsigned _mapd_size_t;
+
+#endif	/* _TYPES_H */
+
