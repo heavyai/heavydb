f3d5601f0569fcc24332e9f0f5df2401f3c36753
Steven Stewart <s26stewa@uwaterloo.ca>
2014-06-23 10:46:12 -0400

    Revamped FileManager.h (chunk-level and block-level API).

--- DataMgr/File/FileMgr.h
+++ DataMgr/File/FileMgr.h
@@ -4,10 +4,10 @@
  * @author  Steven Stewart <steve@map-d.com>
  * @brief   This file contains the class specification and related data structures for FileMgr.
  *
- * This file contains the class specification, and specification for several related
- * types and data structures, for the file manager (FileMgr). This module handles all
- * file system requests from other modules within the DBMS.
+ * This file contains the FileMgr class specification, and the specifications of several related
+ * types and data structures for use by the Map-D file management system.
  *
+ * @see File
  */
 #ifndef FILEMGR_H
 #define FILEMGR_H
@@ -19,83 +19,186 @@
 #include "../../Shared/types.h"
 #include "../../Shared/errors.h"
 
-typedef std::vector <PageInfo> ChunkPages; // holds metadata for all pages in a chunk
-typedef std::map <ChunkKey,ChunkPages> ChunkToPageMap; // ChunkKey is defined in types.h
-typedef std::vector <bool> PageUsedVector;
-
-struct PageAddress {
-    int fileId;
-    mapd_size_t filePageOffset; // in logical pages from beginning of file
-}
-
-struct PageInfo {
-
-    PageAddress pageAddress;
-    mapd_size_t pageSize; // in bytes
-    mapd_size_t endByteOffset; // in bytes from beginning of Page
-    mapd_size_t headerSize; 
-    int keySchemaId; // used to lookup header size
-    //mapd_size_t headerSize; // don't necc need to keep this here - can have seperate
-    unsigned int epoch;
-    /* Next two variables to store location of old version of page if page is
-     * updated so these can be freed at checkpoint time */
-    bool wasDirtied; // should be false at checkpoint; any updates/inserts to page would make this true - meaning the metainfo for the page needs to be updated at checkpoint time.  This might be kept in a seperate data structure for fast (i.e. indexed) access at checkpoint time
-    int oldFileId; //oldFileId = -1 by default to signifiy that page has not been changed 
-    mapd_size_t oldFilePageOffset;
-};
-
-struct PageHeader { //used to read header into 
-    int bytesWritten; //made int so -1 can signify free page
-    unsigned int epoch; 
-    int pageId; //Assumed to be zero based
-    ChunkKey chunkKey;
-}; 
-
-struct FileInfo { 
-    File file;
-    PageUsedVector pageUsedVector; // stores a bit (or boolean) for each page specifying if it is free
-};
+/**
+ * @type ChunkIndexT
+ * @brief An index containing all block addresses associated with a chunk.
+ *
+ * In the indexed allocation approach, each chunk has an index of block addresses
+ * associated with it. The index is implemented, here, as a vector.
+ */
+typedef std::vector<mapd_size_t> ChunkIndexT;
 
 /**
- * @class   FileMgr
- * @author  Todd Mostak <todd@map-d.com>
- * @author  Steven Stewart <steve@map-d.com>
- * 
- * @todo Provide a detailed description of this class.
+ * @type ChunkToIndexT
+ * @brief A map from chunk keys to chunk indices (which contain block addresses).
+ *
+ * A chunk key (ChunkKey) uniquely identifies a chunk in a file, and a chunk
+ * index provides a list of block addresses associated with that chunk. Thus,
+ * ChunkToIndexT is a mapping from chunk keys to chunk indices.
+ */
+typedef std::map<ChunkKey, ChunkIndexT> ChunkToIndexT;
+
+/**
+ * @type _ChunkFileT, ChunkFileT, *ChunkFileTP
+ * @brief A chunk file type has a File object and a ChunkIndexT.
+ *
+ * A chunk file is implemented as a file with a chunk index; hence, the chunk
+ * file type (ChunkFileT) encapsulates both of these. As well, free blocks
+ * in a file must tracked, and this is accomplished using the freeBlocks list.
+ */
+typedef struct _ChunkFileT {
+    File f;
+    ChunkToIndexT index;
+    std::list<mapd_size_t> freeBlocks;
+} ChunkFileT, *ChunkFileTP;
+
+/**
+ * @class FileMgr
+ * @brief The file manager manages interactions between the DBMS and the file system.
+ *
+ *
+ * The main job of the file manager is to translate logical block addresses to physical
+ * block addresses.  It manages a list of (files_), which are actually containers of
+ * chunks, and indexed allocation is used for mapping chunks to block addresses.
+ *
+ * The file manager must also manager free space. It accomplishes this using a linked
+ * list of free block addresses.
+ *
+ * FileMgr provides a Chunk-level API, and also a block-level API for finer granularity. Care
+ * must be taken when using the block-level API such as not to invalidate the indices the map
+ * chunk keys to block addresses.
+ *
+ * @todo buffered writes, asynch vs. synch
+ *
  */
 class FileMgr {
 
-    public:
-        mapd_err_t checkPoint(); // Runs a checkpoint. This involves:
-                                 // 1. Telling buffer pool to flush all dirty
-                                 // pages (maybe done externally so FileManager
-                                 // does not need reference to buffer manager
-                                 // 2. Marking as free old versions of all
-                                 //    pages that have been updated since last
-                                 //    epoch
-                                 // 3. Incrementing epoch counter and flushing
-                                 //    this to disk
+public:
+    FileMgr();
+    ~FileMgr();
+    
+    // ***** CHUNK INTERFACE *****
+    
+    /**
+     *
+     *
+     * @param fileId
+     * @param key
+     * @param index
+     * @return
+     */
+    mapd_err_t getChunkIndex(const int fileId, const ChunkKey &key, ChunkIndexT &index) const;
 
-        mapd_err_t getChunkSize (const ChunkKey &chunkKey, mapd_size_t &chunkSize) const; //Returns byte size of chunk (truncated to last record)
-        mapd_err_t copyChunkToMemory(const ChunkKey &chunkKey, void *mem) const; //Always copies newest version of pages in chunk in contiguous pageId order to memory - usually requested by buffer pool
-        mapd_err_t deleteChunk (const ChunkKey &chunkKey); // Needs to update chunkToPageMap - can't free page until    
-        mapd_err_t flushPageToDisk (const ChunkKey &chunkKey, const int pageId, const void *mem); //can you flush a new page or do you have to create it first
-        mapd_err_t createPage (const ChunkKey &chunkKey, int &pageId); // returns pageId of newly created page by reference
-
-    private:
-
-        std::string basePath_; 
-        std::vector<FileInfo> files_;
-        std::vector<mapd_size_t> keySchemaSizes_; // stores size of keySchema headers in bytes - implicitly ordered by KeySchema id - starting at 0
-        std::vector<int> d
-        ChunkToPageMap chunkToPageMap_;
-        PageUsedVector pageUsedVector_;
-        unsigned int epoch_; // where is this stored on disk - in file header?
-
-        mapd_err_t readHeaderFromFile (const int fileId, const int filePageOffset, PageHeader &pageHeader) const;
-        mapd_err_t getFilenameFromId(const int fileId, std::string &fileName) const; // returns filename by reference
-        mapd_err_t getNumDatFilesInDir(const string &basePath, mapd_size_t &numDataFiles) const; //base path may but does not have to be class member variable basePath_
-
+   /**
+    * @param fileId
+    * @param key
+    * @param buf
+    * @return
+    */
+    mapd_err_t getChunk(const int fileId, const ChunkKey &key, void *buf) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param key
+    * @param nblocks
+    * @return
+    */
+    mapd_err_t getChunkSize(const int fileId, const ChunkKey &key, int *nblocks) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param key
+    * @param size
+    * @return
+    */
+    mapd_err_t getChunkSize(const int fileId, const ChunkKey &key, mapd_size_t *size) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param key
+    * @param nblocks
+    * @param size
+    * @return
+    */
+    mapd_err_t getChunkSize(const int fileId, const ChunkKey &key, int *nblocks, mapd_size_t *size) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param key
+    * @param requested
+    * @param nblocks
+    * @return
+    */
+    mapd_err_t createChunk(const int fileId, ChunkKey &key, const int requested, int *nblocks);
+    
+   /**
+    *
+    * @param fileId
+    * @param key
+    * @return
+    */
+    mapd_err_t deleteChunk(const int fileId, const ChuckKey &key);
+    
+   /**
+    *
+    * @param fileId
+    * @param keys
+    * @return
+    */
+    mapd_err_t deleteChunk(const int fileId, const vector<ChuckKey> &keys);
+    
+    
+    // ***** BLOCK INTERFACE *****
+    
+   /**
+    *
+    * @param fileId
+    * @param blockNum
+    * @param buf
+    * @return
+    */
+    mapd_err_t getBlock(const int fileId, const mapd_size_t blockNum, void *buf) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param blockNums
+    * @param buf
+    * @return
+    */
+    mapd_err_t getBlock(const int fileId, const std::vector<mapd_size_t> blockNums, void *buf) const;
+    
+   /**
+    *
+    * @param fileId
+    * @param blockNum
+    * @return
+    */
+    mapd_err_t createBlock(const int fileId, mapd_size_t *blockNum);
+    
+   /**
+    *
+    * @param fileId
+    * @param blockNum
+    * @return
+    */
+    mapd_err_t deleteBlock(const int fileId, mapd_size_t blockNum);
+    
+   /**
+    *
+    * @param fileId
+    * @param index
+    * @return
+    */
+    mapd_err_t deleteBlock(const int fileId, const ChunkIndexT &index);
+    
+private:
+    std::string basePath_;              /**< The OS file system path containing the files. */
+    std::vector<ChunkFileT> files_;     /**< The vector of files of chunks being managed. */
 };
 
 #endif // FILEMGR_H
