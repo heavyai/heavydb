c4da4e3fefd5cd6d3b2d45eec2d4dcc1f0d85755
Todd Mostak <todd@mapd.com>
2014-07-16 05:54:41 -0400

    Now compiles Database and TcpSever classes across directories - database starts io_service with specified number of threads and then invokes tcpserver with this - database also handles signal catching and has framework to shut other modules down - TcpServer successfully can take input and hand it to database, next step is to link to parser

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,4 +1,59 @@
-Database::Database(const int tcpPort, const int numThreads): tcpPort_(tcpPort), numThreads_(numThreads), tcpServer_("0.0.0.0",tcpPort,  { 
+#include <iostream>
+#include "Database.h"
 
+using namespace std;
+
+namespace Database_Namespace {
+
+Database::Database(const std::string &tcpPort, const int numThreads): tcpPort_(tcpPort), numThreads_(numThreads), signals_(ioService_),  tcpServer_("0.0.0.0",tcpPort,ioService_, *this)   { 
+    registerSignals();
+}
+
+void Database::registerSignals() {
+  // Register to handle the signals that indicate when the server should exit.
+  // It is safe to register for the same signal multiple times in a program,
+  // provided all registration for the specified signal is made through Asio.
+  cout << "Registering signals" << endl;
+  signals_.add(SIGINT);
+  signals_.add(SIGTERM);
+#if defined(SIGQUIT)
+  signals_.add(SIGQUIT);
+#endif // defined(SIGQUIT)
+  signals_.async_wait(boost::bind(&Database::stop, this));
+}
+
+void Database::start() {
+  // Create a pool of threads to run all of the io_services.
+  std::vector<boost::shared_ptr<boost::thread> > threads;
+  for (std::size_t i = 0; i < numThreads_; ++i)
+  {
+    boost::shared_ptr<boost::thread> thread(new boost::thread(
+          boost::bind(&boost::asio::io_service::run, &ioService_)));
+    threads.push_back(thread);
+  }
+
+  tcpServer_.start();
+
+  // Wait for all threads in the pool to exit.
+  // use join_all?
+  for (std::size_t i = 0; i < threads.size(); ++i)
+    threads[i]->join();
+}
+
+void Database::stop() {
+    std::cout << "Caught a kill signal - database stopping" << std::endl;
+    tcpServer_.stop();
+
+    // would need to checkpoint here
+
+    ioService_.stop();
+}
+
+bool Database::processRequest(const std::string &request) {
+    std::cout << "Request: " << request << std::endl;
+    return true;
+}
+        
+} // Database_Namespace
 
 
--- Server/TcpServer/TcpConnection.cpp
+++ Server/TcpServer/TcpConnection.cpp
@@ -1,21 +1,22 @@
-#include "TcpConnection.h"
 #include <vector>
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
+
+#include "TcpConnection.h"
+#include "../../Database/Database.h"
 
 // DEBUG ONLY FIXME
 #include <sstream>
 #include <iostream>
 #include <string>
-//
 
-namespace TcpServer {
+namespace TcpServer_Namespace {
 
-TcpConnection::TcpConnection(boost::asio::io_service& io_service,
-    RequestHandler& handler)
-  : strand_(io_service),
-    socket_(io_service),
-    requestHandler_(handler),
+TcpConnection::TcpConnection(boost::asio::io_service& ioService,
+    Database& database)
+  : strand_(ioService),
+    socket_(ioService),
+    database_(database),
     queryDelim_(';')
 {
 }
@@ -56,17 +57,11 @@ void TcpConnection::handle_read(const boost::system::error_code& e,
   if (!e)
   {
 
-    bool isLegit;
-    //std::ostringstream ss;
     const char* string = boost::asio::buffer_cast<const char*>(buffer_.data());
-    std::string theReq(string, bytes_transferred);
+    std::string request(string, bytes_transferred);
     buffer_.consume(buffer_.size());
-    std::cout << "TcpConnection::handle_read() : " << theReq << std::endl;
-    isLegit = requestHandler_.parse(request_, theReq); //.begin(), buffer_.begin() + bytes_transferred);
-    isLegit = true;
-
-    // Request is good
-    if (isLegit) {
+    bool isValid = database_.processRequest(request);
+    if (isValid) { // Request is good
         char data [24];
         int bytes = 16;
         long long int numRows = -1;
@@ -81,8 +76,6 @@ void TcpConnection::handle_read(const boost::system::error_code& e,
         bytes = 0;
         memcpy(data + 20, &bytes,sizeof(int));
         
-
-
         /*
       std::ostringstream ss;
       ss << boost::lexical_cast<std::string>(buffer_);
@@ -152,4 +145,4 @@ void TcpConnection::handle_write(const boost::system::error_code& e)
   // destructor closes the socket.
 }
 
-} // namespace TcpServer
+} // namespace TcpServer_Namespace
