6c2354bd6d394d7388b0caeebb661a67d967420a
Steven Stewart <s26stewa@uwaterloo.ca>
2014-07-18 10:57:40 -0400

    Handy macros for debugging.

---
+++ Shared/macros.h
@@ -0,0 +1,3 @@
+#define PRINT_SLINE(n) for (int i = 0; i < n; ++i) printf("-"); printf("\n");
+#define PRINT_DLINE(n) for (int i = 0; i < n; ++i) printf("="); printf("\n");
+#define PRINT_DEBUG(msg) printf("[%s:%d] %s\n", __func__, __LINE__, msg);
