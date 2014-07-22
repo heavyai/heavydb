00d8b9c4a935f872eec0a17503e427e95e432b29
Steven Stewart <s26stewa@uwaterloo.ca>
2014-07-22 12:05:33 -0400

    Fixed an invalid header file name in RAParse.cpp

--- QueryEngine/Parse/RAParse.cpp
+++ QueryEngine/Parse/RAParse.cpp
@@ -1,12 +1,12 @@
 #include "Parse.h"
 #include "RA/parser.h"
-#include "RA/ast/RA_Program.h"
+#include "RA/ast/Program.h"
 
 using namespace Parse_Namespace;
 using namespace RA_Namespace;
 
 std::pair<bool, RelAlgNode*> RAParse::parse(const std::string &input, std::string &errMsg) {
-    Parser parser;
+    RAParser parser;
     RelAlgNode *parseRoot = 0;
     std::string lastParsed;
     int numErrors = parser.parse(input, parseRoot, lastParsed);
