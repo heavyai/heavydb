07890822b2a946c1d6c1b9ec10aa855d2e4e72b0
Steven Stewart <s26stewa@uwaterloo.ca>
2014-07-24 08:08:11 -0400

    Renamed and moved the type checker to the visitor subdirectory.

--- QueryEngine/Parse/SQL/compile.sh
+++ QueryEngine/Parse/SQL/compile.sh
@@ -1,4 +1,4 @@
 bison++ -d -hparser.h -o parser.cpp parser.y
 flex++ -d -i -oscanner.cpp scanner.l
 g++ scanner.cpp parser.cpp visitorTest.cpp -o SQLParser -w
-g++ scanner.cpp parser.cpp CheckerTest.cpp -o typeChecker -w
+g++ scanner.cpp parser.cpp typeCheckTest.cpp -o typeChecker -w
