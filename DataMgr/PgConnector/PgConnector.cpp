faa5f9a311ec5cb13b29605dfc47184b7ee72863
Steven Stewart <s26stewa@uwaterloo.ca>
2014-08-13 12:28:52 -0700

    Removed unused variable queryError.

--- DataMgr/PgConnector/PgConnector.cpp
+++ DataMgr/PgConnector/PgConnector.cpp
@@ -1,6 +1,6 @@
 /**
- * @file		PgConnector.cpp
- * @author		Todd Mostak <todd@map-d.com>
+ * @file        PgConnector.cpp
+ * @author      Todd Mostak <todd@map-d.com>
  */
 
 #include "PgConnector.h"
@@ -10,11 +10,10 @@
 using namespace std;
 
 
-PgConnector::PgConnector (const std::string &dbName, const std::string &userName, const std::string &host, const std::string &port): dbName_(dbName), userName_(userName), host_(host), port_(port),  connectString_("port=" + port + " dbname=" + dbName  + " user=" + userName), conn_(connectString_), numRows_(0), numCols_(0) {} 
+PgConnector::PgConnector (const std::string &dbName, const std::string &userName, const std::string &host, const std::string &port): dbName_(dbName), userName_(userName), host_(host), port_(port),  connectString_("port=" + port + " dbname=" + dbName  + " user=" + userName), conn_(connectString_), numRows_(0), numCols_(0) {}
 
-mapd_err_t PgConnector::query(const std::string &queryString) { 
+mapd_err_t PgConnector::query(const std::string &queryString) {
     pqxx::work txn (conn_);
-    int queryError = 0;
     try {
         pgResults_ = txn.exec(queryString);
     }
@@ -30,19 +29,19 @@ mapd_err_t PgConnector::query(const std::string &queryString) {
 }
 
 /*
-int main() {
-    cout << "Hello" << endl;
-    PgConnector pgConnector("mapd","tmostak","127.0.0.1","5432"); 
-    //////////////////mapd_err_t status = pgConnector.query("select * from us_zip"); 
-    mapd_err_t status = pgConnector.query("select table_id, fragment_id, num_rows from fragments"); 
-    cout << "Status: " << status << endl;
-    if (status == MAPD_SUCCESS) {
-        int numRows = pgConnector.getNumRows();
-        for (int r = 0; r != numRows; ++r)
-            cout << pgConnector.getData <int> (r,0) << " " << pgConnector.getData<int> (r,1) << " " << pgConnector.getData<int> (r,2) << endl;
-    }
-
-
-};
-*/
+ int main() {
+ cout << "Hello" << endl;
+ PgConnector pgConnector("mapd","tmostak","127.0.0.1","5432");
+ //////////////////mapd_err_t status = pgConnector.query("select * from us_zip");
+ mapd_err_t status = pgConnector.query("select table_id, fragment_id, num_rows from fragments");
+ cout << "Status: " << status << endl;
+ if (status == MAPD_SUCCESS) {
+ int numRows = pgConnector.getNumRows();
+ for (int r = 0; r != numRows; ++r)
+ cout << pgConnector.getData <int> (r,0) << " " << pgConnector.getData<int> (r,1) << " " << pgConnector.getData<int> (r,2) << endl;
+ }
+ 
+ 
+ };
+ */
 
