644d12d083aced936c18c695a943d8c53111b8e6
Steven Stewart <s26stewa@uwaterloo.ca>
2014-08-22 17:11:19 -0400

    Initial commit of NameWalker. More to come.

---
+++ QueryEngine/Analysis/nameWalkerTest.cpp
@@ -0,0 +1,9 @@
+//
+//  nameWalkerTest.cpp
+//  mapd2
+//
+//  Created by Steven Stewart on 8/19/14.
+//  Copyright (c) 2014 Map-D Technologies, Inc. All rights reserved.
+//
+
+#include "nameWalkerTest.h"
