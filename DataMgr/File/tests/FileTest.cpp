aa0de67dea41ff9e656aeae66180a587eb3b0715
Steven Stewart <s26stewa@uwaterloo.ca>
2014-08-26 10:26:21 -0400

    Added a new unit test for read/write.

--- DataMgr/File/tests/FileTest.cpp
+++ DataMgr/File/tests/FileTest.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <ctime>
 #include "gtest/gtest.h"
 #include "../File.h"
 
@@ -8,7 +10,7 @@ GTEST_API_ int main(int argc, char **argv) {
     return RUN_ALL_TESTS();
 }
 
-TEST(File, create)
+TEST(File, CreateOpenClose)
 {
     // test invalid arguments
     EXPECT_THROW(create(0, 4096, 0), std::invalid_argument);
@@ -26,5 +28,40 @@ TEST(File, create)
     ASSERT_NO_THROW(f = open(fileId));
     EXPECT_EQ(fileSize(f), blockSize * nblocks);
     EXPECT_NO_THROW(close(f));
+}
+
+TEST(File, ReadWrite)
+{
+    int fileId = 0;
+    int blockSize = 4096;
+    int nblocks = 1;
+    int numInts = (blockSize * nblocks) / sizeof(int);
+    int randUpper = 10;
+    
+    // initialize buffers with random integers
+    srand((unsigned)time(0));
+    int *A = new int[numInts];
+    int *B = new int[numInts];
+    for (int i = 0; i < numInts; ++i) {
+        A[i] = (rand()%randUpper)+1;
+        B[i] = -1;
+    }
+    
+    // create a file
+    FILE *f = create(fileId, blockSize, nblocks);
+    
+    // write buffer to the file
+    size_t bytesWritten = write(f, 0, blockSize * nblocks, (mapd_addr_t)A);
+    ASSERT_EQ(bytesWritten, blockSize * nblocks);
+    
+    // read buffer
+    read(f, 0, blockSize * nblocks, (mapd_addr_t)B);
+    
+    // compare buffers
+    for (int i = 0; i < numInts; ++i)
+        ASSERT_EQ(A[i], B[i]);
+    
+    delete[] A;
+    delete[] B;
 }
 
