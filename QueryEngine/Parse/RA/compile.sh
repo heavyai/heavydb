d455960e94a9048915699ff2ed6501b5fbdc74c7
Steven Stewart <s26stewa@uwaterloo.ca>
2014-09-25 17:35:51 -0700

    Updates to RA parser tests and ra2xml.

--- QueryEngine/Parse/RA/compile.sh
+++ QueryEngine/Parse/RA/compile.sh
@@ -1,4 +1,3 @@
 bison++ -d -hparser.h -o parser.cpp parser.y
 flex++ -d -i -oscanner.cpp scanner.l
-g++ scanner.cpp parser.cpp tests/visitorTest.cpp -I/usr/local/include -L/usr/local/lib/ -lpqxx -o visitorTest -w -g -std=c++11
-
+g++ scanner.cpp parser.cpp tests/ra2xml.cpp visitor/XMLTranslator.cpp -I/usr/local/include -L/usr/local/lib/ -lpqxx -o ra2xml -w -g -std=c++11
