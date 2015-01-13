aea9e8011a41abc8cceaeba4038b974936880d24
Alex Șuhan <alex.suhan@gmail.com>
2015-01-12 22:06:08 -0800

    Remove unused sub-folders in QueryEngine

--- QueryEngine/Execution/compile.sh
+++
@@ -1,2 +0,0 @@
-clear
-g++ -g -std=c++11 -o testQPChainingExec testQPChainingExec.cpp QPChainingExec.cpp ../Parse/RA/parser.cpp ../Parse/RA/scanner.cpp -w
--- QueryEngine/Execution/compileirgen.sh
+++
@@ -1,2 +0,0 @@
-clear
-g++ -g -std=c++11 -o testQPIRGen testQPIRGeneration.cpp QPIrGenerator.cpp QPIRPrepper.cpp ../Parse/RA/parser.cpp ../Parse/RA/scanner.cpp -I/usr/local/include -L/usr/local/lib -lpqxx `llvm-config --cppflags --ldflags --libs` -w 
