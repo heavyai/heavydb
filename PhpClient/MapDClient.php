a5da3a75763df20a8c674a3b492fb3c381fb4cbe
Alex Șuhan <alex.suhan@gmail.com>
2015-02-13 18:01:28 -0800

    PHP client example

---
+++ PhpClient/MapDClient.php
@@ -0,0 +1,34 @@
+<?php
+
+// Setup the path to the thrift library folder
+$GLOBALS['THRIFT_ROOT'] = './Thrift';
+ 
+// Load up all the thrift stuff
+require_once $GLOBALS['THRIFT_ROOT'].'/Base/TBase.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Exception/TException.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Exception/TApplicationException.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/TStringFunc.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/Core.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Factory/TStringFuncFactory.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Protocol/TProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Protocol/TBinaryProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TTransport.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TBufferedTransport.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TSocket.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Type/TMessageType.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Type/TType.php';
+ 
+require_once $GLOBALS['THRIFT_ROOT'].'/gen-php/Types.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/gen-php/MapD.php';
+
+$socket = new Thrift\Transport\TSocket('localhost', '9090');
+$transport = new Thrift\Transport\TBufferedTransport($socket);
+$protocol = new Thrift\Protocol\TBinaryProtocol($transport);
+
+// Create a calculator client
+$client = new MapDClient($protocol);
+
+// Open up the connection
+$transport->open();
+
+var_dump($client->select("SELECT COUNT(*) FROM test;"));
