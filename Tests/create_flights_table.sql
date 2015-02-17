c25db63d04cd1bf504a474bf2162996ba089e295
Alex Șuhan <alex.suhan@gmail.com>
2015-02-16 17:15:11 -0800

    Add Flights data files

---
+++ Tests/create_flights_table.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Flights(Year smallint, Month smallint, DayofMonth smallint, DayOfWeek smallint, DepTime smallint, CRSDepTime smallint, ArrTime smallint, CRSArrTime smallint, UniqueCarrier text);
+
