45d1b859e3f55bcd06df2276ef4e57609c739bca
Wei Hong <wei@map-d.com>
2015-03-04 11:07:35 -0800

    forgot to git add

---
+++ Utils/ChunkIter.h
@@ -0,0 +1,28 @@
+/**
+ * @file ChunkIter.h
+ * @author Wei Hong <wei@mapd.com>
+ *
+ */
+#ifndef _CHUNK_ITER_H_
+#define _CHUNK_ITER_H_
+
+#include "../Shared/sqltypes.h"
+
+struct ChunkIter {
+    const SQLTypeInfo *type_info;
+    int8_t *second_buf;
+		int8_t *current_pos;
+		int8_t *start_pos;
+		int8_t *end_pos;
+		int skip;
+		int skip_size;
+    size_t num_elems;
+		Datum datum; // used to hold uncompressed value
+};
+
+void ChunkIter_reset(ChunkIter *it);
+void ChunkIter_get_next(ChunkIter *it, bool uncompress, VarlenDatum *vd, bool *is_end);
+// @brief get nth element in Chunk.  Does not change ChunkIter state
+void ChunkIter_get_nth(ChunkIter *it, size_t nth, bool uncompress, VarlenDatum *vd, bool *is_end);
+
+#endif // _CHUNK_ITER_H_
