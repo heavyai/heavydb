6a8aa537982f58b766ebabe5bccc4df0556c2219
Wei Hong <wei@map-d.com>
2015-06-16 12:28:43 -0700

    add commandline options for configuring StreamInsert

--- SampleCode/StreamInsert.cpp
+++ SampleCode/StreamInsert.cpp
@@ -16,6 +16,8 @@
 #include <thread>
 #include <chrono>
 
+#include <boost/program_options.hpp>
+
 // include files for Thrift and MapD Thrift Services
 #include "gen-cpp/MapD.h"
 #include <thrift/transport/TSocket.h>
@@ -29,24 +31,26 @@ using namespace ::apache::thrift::transport;
 // Thrift uses boost::shared_ptr instead of std::shared_ptr
 using boost::shared_ptr;
 
+struct CopyParams {
+  char delimiter;
+  std::string null_str;
+  char line_delim;
+  size_t batch_size;
+  CopyParams(char d, const std::string &n, char l, size_t b) : delimiter(d), null_str(n), line_delim(l), batch_size(b) {}
+};
+
+const bool print_error_data = false;
+const bool print_transformation = false;
+
+
 namespace {
   // anonymous namespace for private functions
-  struct CopyParams {
-    char delimiter;
-    std::string null_str;
-    char line_delim;
-    size_t batch_size;
-    CopyParams() : delimiter('\x01'), null_str("\\N"), line_delim('\n'), batch_size(10000) {}
-  } copy_params;
 
 #define MAX_FIELD_LEN   20000
-  const bool print_error_data = false;
-  const bool print_transformation = false;
-
   // reads copy_params.delimiter delimited rows from std::cin and load them to
   // table_name in batches of size copy_params.batch_size until EOF
   void
-  stream_insert(MapDClient &client, const TSessionId session, const std::string &table_name, const TRowDescriptor &row_desc, const std::map<std::string, std::pair<std::unique_ptr<std::regex>, std::unique_ptr<std::string>>> &transformations)
+  stream_insert(MapDClient &client, const TSessionId session, const std::string &table_name, const TRowDescriptor &row_desc, const std::map<std::string, std::pair<std::unique_ptr<std::regex>, std::unique_ptr<std::string>>> &transformations, const CopyParams &copy_params)
   {
     std::vector<TStringRow> input_rows;
     TStringRow row;
@@ -187,54 +191,134 @@ namespace {
 int main(int argc, char **argv) {
   std::string server_host("localhost"); // default to localohost
   int port = 9091; // default port number
+  std::string table_name;
+  std::string db_name;
+  std::string user_name;
+  std::string passwd;
+  std::string delim_str(","), nulls("\\N"), line_delim_str("\n");
+  size_t batch_size = 10000;
+  std::vector<std::string> xforms;
   std::map<std::string, std::pair<std::unique_ptr<std::regex>, std::unique_ptr<std::string>>> transformations;
 
-  if (argc < 5) {
-    std::cout << "Usage: <table> <database> <user> <password> [hostname[:port]] [transformation ...]" << std::endl;
-    return 1;
-  }
-  std::string table_name(argv[1]);
-  std::string db_name(argv[2]);
-  std::string user_name(argv[3]);
-  std::string passwd(argv[4]);
+	namespace po = boost::program_options;
 
-  if (argc >= 6) {
-    char *host = strtok(argv[5], ":");
-    char *portno = strtok(NULL, ":");
-    server_host = host;
-    if (portno != NULL)
-      port = atoi(portno);
+	po::options_description desc("Options");
+	desc.add_options()
+		("help,h", "Print help messages ")
+		("table", po::value<std::string>(&table_name)->required(), "Table Name")
+		("database", po::value<std::string>(&db_name)->required(), "Database Name")
+		("user,u", po::value<std::string>(&user_name)->required(), "User Name")
+		("passwd,p", po::value<std::string>(&passwd)->required(), "User Password")
+    ("host", po::value<std::string>(&server_host), "MapD Server Hostname")
+    ("port", po::value<int>(&port), "MapD Server Port Number")
+    ("delim", po::value<std::string>(&delim_str), "Field delimiter")
+    ("null", po::value<std::string>(&nulls), "NULL string")
+    ("line", po::value<std::string>(&line_delim_str), "Line delimiter")
+    ("batch", po::value<size_t>(&batch_size), "Insert batch size")
+    ("transform,t", po::value<std::vector<std::string>>(&xforms)->multitoken(), "Column Transformations");
+
+	po::positional_options_description positionalOptions;
+	positionalOptions.add("table", 1);
+	positionalOptions.add("database", 1);
+
+	po::variables_map vm;
+
+	try {
+		po::store(po::command_line_parser(argc, argv).options(desc).positional(positionalOptions).run(), vm);
+		if (vm.count("help")) {
+			std::cout << "Usage: <table name> <database name> {-u|--user} <user> {-p|--passwd} <password> [{--host} <hostname>][--port <port number>][--delim <delimiter>][--null <null string>][--line <line delimiter>][--batch <batch size>][{-t|--transform} transformation ...]\n";
+			return 0;
+		}
+
+		po::notify(vm);
+	}
+	catch (boost::program_options::error &e)
+	{
+		std::cerr << "Usage Error: " << e.what() << std::endl;
+		return 1;
+	}
+
+  char delim = delim_str[0];
+  if (delim == '\\') {
+    if (delim_str.size() < 2 || (delim_str[1] != 'x' && delim_str[1] != 't' && delim_str[1] != 'n')) {
+      std::cerr << "Incorrect delimiter string: " << delim_str << std::endl;
+      return 1;
+    }
+    if (delim_str[1] == 't')
+      delim = '\t';
+    else if (delim_str[1] == 'n')
+      delim = '\n';
+    else {
+      std::string d(delim_str);
+      d[0] = '0';
+      delim = (char)std::stoi(d, nullptr, 16);
+    }
   }
-  for (int i = 6; i < argc; i++) {
-    char *col_name = argv[i];
-    char *p;
-    for (p = col_name; *p != ':' && *p != '\0'; ++p);
-    if (*p == '\0') {
+  if (isprint(delim))
+    std::cout << "Field Delimiter: " << delim << std::endl;
+  else if (delim == '\t')
+    std::cout << "Line Delimiter: " << "\\t" << std::endl;
+  else if (delim == '\n')
+    std::cout << "Line Delimiter: " << "\\n" << std::endl;
+  else
+    std::cout << "Field Delimiter: \\x" << std::hex << (int)delim << std::endl;
+  char line_delim = line_delim_str[0];
+  if (line_delim == '\\') {
+    if (line_delim_str.size() < 2 || (line_delim_str[1] != 'x' && line_delim_str[1] != 't' && line_delim_str[1] != 'n')) {
+      std::cerr << "Incorrect delimiter string: " << line_delim_str << std::endl;
+      return 1;
+    }
+    if (line_delim_str[1] == 't')
+      line_delim = '\t';
+    else if (delim_str[1] == 'n')
+      line_delim = '\n';
+    else {
+      std::string d(delim_str);
+      d[0] = '0';
+      line_delim = (char)std::stoi(d, nullptr, 16);
+    }
+  }
+  if (isprint(line_delim))
+    std::cout << "Line Delimiter: " << line_delim << std::endl;
+  else if (line_delim == '\t')
+    std::cout << "Line Delimiter: " << "\\t" << std::endl;
+  else if (line_delim == '\n')
+    std::cout << "Line Delimiter: " << "\\n" << std::endl;
+  else
+    std::cout << "Line Delimiter: \\x" << std::hex << (int)line_delim << std::endl;
+  std::cout << "Null String: " << nulls << std::endl;
+  std::cout << "Insert Batch Size: " << std::dec << batch_size << std::endl;
+
+  for (auto &t : xforms) {
+    auto n = t.find_first_of(':');
+    if (n == std::string::npos) {
       std::cerr << "Transformation format: <column name>:s/<regex pattern>/<fmt string>/" << std::endl;
       return 1;
     }
-    *(p++) = '\0';
-    if (*(p++) != 's' || *(p++) != '/') {
+    std::string col_name = t.substr(0, n);
+    if (t.size() < n + 3 || t[n+1] != 's' || t[n+2] != '/') {
       std::cerr << "Transformation format: <column name>:s/<regex pattern>/<fmt string>/" << std::endl;
       return 1;
     }
-    char *regex_str = p;
-    for (; (*p != '/' || *(p - 1) == '\\') && *p != '\0'; ++p);
-    if (*p == '\0') {
+    auto n1 = n + 3;
+    auto n2 = t.find_first_of('/', n1);
+    if (n2 == std::string::npos) {
       std::cerr << "Transformation format: <column name>:s/<regex pattern>/<fmt string>/" << std::endl;
       return 1;
     }
-    *(p++) = '\0';
-    char *fmt_str = p;
-    for (; (*p != '/' || *(p - 1) == '\\') && *p != '\0'; ++p);
-    if (*p == '\0') {
+    std::string regex_str = t.substr(n1, n2-n1);
+    n1 = n2 + 1;
+    n2 = t.find_first_of('/', n1);
+    if (n2 == std::string::npos) {
       std::cerr << "Transformation format: <column name>:s/<regex pattern>/<fmt string>/" << std::endl;
       return 1;
     }
-    *p = '\0';
+    std::string fmt_str = t.substr(n1, n2-n1);
     std::cout << "transform " << col_name << ": s/" << regex_str << "/" << fmt_str << "/" << std::endl;
     transformations[col_name] = std::pair<std::unique_ptr<std::regex>, std::unique_ptr<std::string>>(std::unique_ptr<std::regex>(new std::regex(regex_str)), std::unique_ptr<std::string>(new std::string(fmt_str)));
   }
+
+  CopyParams copy_params(delim, nulls, line_delim, batch_size);
 
   //for attaching debugger std::this_thread::sleep_for (std::chrono::seconds(20));
 
@@ -248,7 +332,7 @@ int main(int argc, char **argv) {
     session = client.connect(user_name, passwd, db_name); // connect to mapd_server
     TRowDescriptor row_desc;
     client.get_row_descriptor(row_desc, session, table_name);
-    stream_insert(client, session, table_name, row_desc, transformations);
+    stream_insert(client, session, table_name, row_desc, transformations, copy_params);
     client.disconnect(session); // disconnect from mapd_server
     transport->close(); // close transport
   }
