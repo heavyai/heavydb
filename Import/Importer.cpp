27dab4e4f7fce87739da7e9b4d3a563bb0d7b602
Andrew Seidl <dev@aas.io>
2015-08-07 08:10:33 -0700

    Revert "Use boost::trim instead of custom string trim"
    boost::trim causes a major performance regression (5-10x).
    
    This reverts commit e2bb008c7c3d5ad22220871460d616cee865751b.

--- Import/Importer.cpp
+++ Import/Importer.cpp
@@ -62,6 +62,14 @@ Importer::~Importer()
     free(buffer[1]);
 }
 
+static const std::string trim_space(const char* field, const size_t len, const CopyParams& copy_params) {
+  size_t i = 0;
+  size_t j = len;
+  if (field[i] == ' ') i++;
+  if (field[j] == ' ' || field[j] == copy_params.line_delim) j--;
+  return std::string(field + i, j - i);
+}
+
 static const char *
 get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const CopyParams &copy_params, bool is_begin, const bool *is_array, std::vector<std::string> &row)
 {
@@ -86,8 +94,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
     } else if (*p == copy_params.delimiter) {
       if (!in_quote && !in_array) {
         if (!has_escape && !strip_quotes) {
-          std::string s(field, p - field);
-          boost::trim(s);
+          std::string s = trim_space(field, p - field, copy_params);
           row.push_back(s);
         } else {
           char field_buf[p - field + 1];
@@ -105,8 +112,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
             field_buf[j - 1] = '\0';
           else
             field_buf[j] = '\0';
-          std::string s(field_buf, j);
-          boost::trim(s);
+          std::string s = trim_space(field_buf, j, copy_params);
           row.push_back(s);
         }
         field = p + 1;
@@ -116,8 +122,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
     }
   }
   if (*p == copy_params.line_delim) {
-    std::string s(field, p - field);
-    boost::trim(s);
+    std::string s = trim_space(field, p - field, copy_params);
     row.push_back(s);
     return p;
   }
@@ -136,8 +141,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
     } else if (*p == copy_params.delimiter) {
       if (!in_quote && !in_array) {
         if (!has_escape) {
-          std::string s(field, p - field);
-          boost::trim(s);
+          std::string s = trim_space(field, p - field, copy_params);
           row.push_back(s);
         } else {
           char field_buf[p - field + 1];
@@ -155,8 +159,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
             field_buf[j - 1] = '\0';
           else
             field_buf[j] = '\0';
-          std::string s(field_buf, j);
-          boost::trim(s);
+          std::string s = trim_space(field_buf, j, copy_params);
           row.push_back(s);
         }
         field = p + 1;
@@ -166,8 +169,7 @@ get_row(const char *buf, const char *buf_end, const char *entire_buf_end, const 
     } 
   }
   if (*p == copy_params.line_delim) {
-    std::string s(field, p - field);
-    boost::trim(s);
+    std::string s = trim_space(field, p - field, copy_params);
     row.push_back(s);
     return p;
   }
