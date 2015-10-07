6110644563fa5ee1fde2117b1eac43a89694a913
Alex Șuhan <alex.suhan@gmail.com>
2015-10-07 13:11:15 -0700

    Simplify CFG when filter shortcircuit is not needed

--- QueryEngine/Execute.cpp
+++ QueryEngine/Execute.cpp
@@ -3505,12 +3505,14 @@ Executor::CompilationResult Executor::compilePlan(const Planner::Plan* plan,
         cgen_state_->ir_builder_.CreateAnd(filter_lv, toBool(codegen(expr.get(), true, hoist_literals).front()));
   }
 
-  auto sc_true = llvm::BasicBlock::Create(cgen_state_->context_, "sc_true", cgen_state_->row_func_);
-  auto sc_false = llvm::BasicBlock::Create(cgen_state_->context_, "sc_false", cgen_state_->row_func_);
-  cgen_state_->ir_builder_.CreateCondBr(filter_lv, sc_true, sc_false);
-  cgen_state_->ir_builder_.SetInsertPoint(sc_false);
-  cgen_state_->ir_builder_.CreateRet(ll_int(int32_t(0)));
-  cgen_state_->ir_builder_.SetInsertPoint(sc_true);
+  if (!deferred_quals.empty()) {
+    auto sc_true = llvm::BasicBlock::Create(cgen_state_->context_, "sc_true", cgen_state_->row_func_);
+    auto sc_false = llvm::BasicBlock::Create(cgen_state_->context_, "sc_false", cgen_state_->row_func_);
+    cgen_state_->ir_builder_.CreateCondBr(filter_lv, sc_true, sc_false);
+    cgen_state_->ir_builder_.SetInsertPoint(sc_false);
+    cgen_state_->ir_builder_.CreateRet(ll_int(int32_t(0)));
+    cgen_state_->ir_builder_.SetInsertPoint(sc_true);
+  }
 
   for (auto expr : deferred_quals) {
     filter_lv = cgen_state_->ir_builder_.CreateAnd(filter_lv, toBool(codegen(expr, true, hoist_literals).front()));
