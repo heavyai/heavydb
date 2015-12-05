8487fb650f1a6493279786ebe32c680ff5011871
Michael Thomson <michael@mapd.com>
2015-12-04 18:01:41 -0800

    Revert "remove a compiler warning"
    This reverts commit d926bdee435d10d6c737287b13ad01451970cf00.
    
    premature checking broke mac os x

--- QueryEngine/DateTruncate.cpp
+++ QueryEngine/DateTruncate.cpp
@@ -140,6 +140,7 @@ time_t DateTruncate(DatetruncField field, time_t timeval) {
       int millennium_start = ((year-1)/1000)*1000+1;
       return create_epoch(millennium_start);
     }
+      return timeval;
     default:
 #ifdef __CUDACC__
       return -1;
