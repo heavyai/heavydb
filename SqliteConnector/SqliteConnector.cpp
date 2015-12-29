bc59f9df21139490c0403395ab6f6ea5eb6fe3e0
Andrew Seidl <dev@aas.io>
2015-12-29 10:59:50 -0800

    sqlite: add function for multiple text params in prepared stmts

--- SqliteConnector/SqliteConnector.cpp
+++ SqliteConnector/SqliteConnector.cpp
@@ -7,7 +7,10 @@
 #include "SqliteConnector.h"
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+using std::runtime_error;
 
 int resultCallback(void* connObj, int argc, char** argv, char** colNames) {
   cout << "callback" << endl;
@@ -48,7 +51,8 @@ void SqliteConnector::throwError() {
   throw runtime_error("Sqlite3 Error: " + errorMsg);
 }
 
-void SqliteConnector::query_with_text_param(const std::string& queryString, const std::string& text_param) {
+void SqliteConnector::query_with_text_params(const std::string& queryString,
+                                             const std::vector<std::string>& text_params) {
   atFirstResult_ = true;
   numRows_ = 0;
   numCols_ = 0;
@@ -56,56 +60,17 @@ void SqliteConnector::query_with_text_param(const std::string& queryString, cons
   columnTypes.clear();
   results_.clear();
   sqlite3_stmt* stmt;
-  int returnCode = sqlite3_prepare_v2(db_, queryString.c_str(), -1, &stmt, NULL);
+  int returnCode = sqlite3_prepare_v2(db_, queryString.c_str(), -1, &stmt, nullptr);
   if (returnCode != SQLITE_OK) {
     throwError();
   }
 
-  returnCode = sqlite3_bind_text(stmt, 1, text_param.c_str(), text_param.size(), SQLITE_STATIC);
-  if (returnCode != SQLITE_OK)
-    throwError();
-
-  do {
-    returnCode = sqlite3_step(stmt);
-    if (returnCode != SQLITE_ROW && returnCode != SQLITE_DONE) {
+  int numParams_ = 1;
+  for (auto text_param : text_params) {
+    returnCode = sqlite3_bind_text(stmt, numParams_++, text_param.c_str(), text_param.size(), SQLITE_TRANSIENT);
+    if (returnCode != SQLITE_OK) {
       throwError();
     }
-    if (returnCode == SQLITE_DONE) {
-      break;
-    }
-    if (atFirstResult_) {
-      numCols_ = sqlite3_column_count(stmt);
-      for (size_t c = 0; c < numCols_; ++c) {
-        columnNames.push_back(sqlite3_column_name(stmt, c));
-        columnTypes.push_back(sqlite3_column_type(stmt, c));
-      }
-      results_.resize(numCols_);
-      atFirstResult_ = false;
-    }
-    numRows_++;
-    for (size_t c = 0; c < numCols_; ++c) {
-      auto col_text = reinterpret_cast<const char*>(sqlite3_column_text(stmt, c));
-      if (col_text) {
-        results_[c].push_back(
-            col_text);  // b/c sqlite returns unsigned char* which can't be used in constructor of string
-      }
-    }
-  } while (1 == 1);  // Loop control in break statement above
-
-  sqlite3_finalize(stmt);
-}
-
-void SqliteConnector::query(const std::string& queryString) {
-  atFirstResult_ = true;
-  numRows_ = 0;
-  numCols_ = 0;
-  columnNames.clear();
-  columnTypes.clear();
-  results_.clear();
-  sqlite3_stmt* stmt;
-  int returnCode = sqlite3_prepare_v2(db_, queryString.c_str(), -1, &stmt, NULL);
-  if (returnCode != SQLITE_OK) {
-    throwError();
   }
 
   do {
@@ -132,12 +97,20 @@ void SqliteConnector::query(const std::string& queryString) {
         results_[c].push_back(
             col_text);  // b/c sqlite returns unsigned char* which can't be used in constructor of string
       } else {
-        results_[c].push_back(""); // interpret nulls as empty string
+        results_[c].push_back("");  // interpret nulls as empty string
       }
     }
   } while (1 == 1);  // Loop control in break statement above
 
   sqlite3_finalize(stmt);
+}
+
+void SqliteConnector::query_with_text_param(const std::string& queryString, const std::string& text_param) {
+  query_with_text_params(queryString, std::vector<std::string>{text_param});
+}
+
+void SqliteConnector::query(const std::string& queryString) {
+  query_with_text_params(queryString, std::vector<std::string>{});
 }
 
 void SqliteConnector::queryWithCallback(const std::string& queryString) {
