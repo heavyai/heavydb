b4006b7275832b9e5ca48fcc586b76850e5c0921
Alex Șuhan <alex.suhan@gmail.com>
2016-01-22 11:28:25 -0800

    Add some interfaces to the abstract interpreter

--- QueryEngine/RelAlgAbstractInterpreter.cpp
+++ QueryEngine/RelAlgAbstractInterpreter.cpp
@@ -1,1 +1,14 @@
 #include "RelAlgAbstractInterpreter.h"
+
+#include <glog/logging.h>
+
+ScanBufferDesc::ScanBufferDesc() : table_id_(-1) {
+}
+
+ScanBufferDesc::ScanBufferDesc(const int table_id) : table_id_(table_id) {
+  CHECK(table_id_ >= 0);
+}
+
+LoweringInfo ra_interpret(const rapidjson::Value&) {
+  CHECK(false);
+}
--- QueryEngine/RelAlgAbstractInterpreter.h
+++ QueryEngine/RelAlgAbstractInterpreter.h
@@ -1,4 +1,25 @@
 #ifndef QUERYENGINE_RELALGABSTRACTINTERPRETER_H
 #define QUERYENGINE_RELALGABSTRACTINTERPRETER_H
 
+#include <rapidjson/document.h>
+
+class ScanScope {
+  // TODO
+};
+
+class ScanBufferDesc {
+ public:
+  ScanBufferDesc();                    // for results of other queries
+  ScanBufferDesc(const int table_id);  // for tables
+
+ private:
+  const int table_id_;
+};
+
+class LoweringInfo {
+  // TODO
+};
+
+LoweringInfo ra_interpret(const rapidjson::Value&);
+
 #endif  // QUERYENGINE_RELALGABSTRACTINTERPRETER_H
