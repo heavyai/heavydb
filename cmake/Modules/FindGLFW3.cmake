5c070a81f7db3f79a99d6b4366b4de3153720147
Andrew Seidl <dev@aas.io>
2016-02-02 13:17:19 -0800

    cmake: static link GLFW3, link against req'd X11 libs

--- cmake/Modules/FindGLFW3.cmake
+++ cmake/Modules/FindGLFW3.cmake
@@ -22,10 +22,15 @@
 #   GLFW3_FOUND - true if GLFW3 has been found and can be used
 
 find_path(GLFW3_INCLUDE_DIR GLFW/glfw3.h)
-find_library(GLFW3_LIBRARY NAMES glfw glfw3 PATH_SUFFIXES lib64)
+find_library(GLFW3_LIBRARY NAMES glfw3 glfw PATH_SUFFIXES lib64)
 
 set(GLFW3_INCLUDE_DIRS ${GLFW3_INCLUDE_DIR})
 set(GLFW3_LIBRARIES ${GLFW3_LIBRARY})
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  find_package(X11)
+  list(APPEND GLFW3_LIBRARIES ${X11_X11_LIB} ${X11_Xxf86vm_LIB} ${X11_Xcursor_LIB} ${X11_Xrandr_LIB} ${X11_Xinerama_LIB})
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLFW3 REQUIRED_VARS GLFW3_INCLUDE_DIR GLFW3_LIBRARY)
