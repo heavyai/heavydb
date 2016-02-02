c5cc43df33c78fd071aaba1278689137976dc93b
Andrew Seidl <dev@aas.io>
2016-02-02 11:08:40 -0800

    systemd: add render support, fixup install script

--- systemd/install_mapd_systemd.sh
+++ systemd/install_mapd_systemd.sh
@@ -7,18 +7,13 @@ vars["MAPD_PATH"]=${MAPD_PATH:=$(dirname $(pwd))}
 
 descs["MAPD_STORAGE"]="MapD data and configuration storage directory"
 vars["MAPD_STORAGE"]=${MAPD_STORAGE:="/var/lib/mapd"}
-vars["MAPD_DATA"]=${MAPD_DATA:="${MAPD_STORAGE}/data"}
 
 descs["MAPD_USER"]="user MapD will be run as"
 vars["MAPD_USER"]=${MAPD_USER:=$(id --user --name)}
 descs["MAPD_GROUP"]="group MapD will be run as"
 vars["MAPD_GROUP"]=${MAPD_GROUP:=$(id --group --name)}
 
-descs["CUDA_HOME"]="CUDA Toolkit install directory"
-vars["CUDA_HOME"]=${CUDA_HOME:="/usr/local/cuda"}
-
-
-for v in MAPD_PATH MAPD_STORAGE MAPD_USER MAPD_GROUP CUDA_HOME; do
+for v in MAPD_PATH MAPD_STORAGE MAPD_USER MAPD_GROUP; do
   echo "$v: ${descs["$v"]}"
   read -p "[${vars[$v]}]: "
   if [ ! -z "$REPLY" ]; then
@@ -27,12 +22,15 @@ for v in MAPD_PATH MAPD_STORAGE MAPD_USER MAPD_GROUP CUDA_HOME; do
   echo
 done
 
-for v in MAPD_PATH MAPD_STORAGE MAPD_USER MAPD_GROUP CUDA_HOME; do
+for v in MAPD_PATH MAPD_STORAGE MAPD_USER MAPD_GROUP; do
   echo -e "$v:\t${vars[$v]}"
 done
 
-sudo mkdir -p ${vars['MAPD_DATA']}
-sudo mkdir -p ${vars['MAPD_STORAGE']}
+vars["MAPD_DATA"]=${MAPD_DATA:="${vars['MAPD_STORAGE']}/data"}
+sudo mkdir -p "${vars['MAPD_DATA']}"
+sudo mkdir -p "${vars['MAPD_STORAGE']}"
+sudo chown -R ${vars['MAPD_USER']}:${vars['MAPD_GROUP']} "${vars['MAPD_DATA']}"
+sudo chown -R ${vars['MAPD_USER']}:${vars['MAPD_GROUP']} "${vars['MAPD_STORAGE']}"
 
 for f in mapd_server mapd_server@ mapd_web_server mapd_web_server@ ; do
   sed -e "s#@MAPD_PATH@#${vars['MAPD_PATH']}#g" \
@@ -40,18 +38,25 @@ for f in mapd_server mapd_server@ mapd_web_server mapd_web_server@ ; do
       -e "s#@MAPD_DATA@#${vars['MAPD_DATA']}#g" \
       -e "s#@MAPD_USER@#${vars['MAPD_USER']}#g" \
       -e "s#@MAPD_GROUP@#${vars['MAPD_GROUP']}#g" \
-      -e "s#@CUDA_HOME@#${vars['CUDA_HOME']}#g" \
       $f.service.in > $f.service
   sudo cp $f.service /lib/systemd/system/
 done
+if [ -f mapd_xorg.service ]; then
+	sudo cp mapd_xorg.service /lib/systemd/system/
+fi
 
 sed -e "s#@MAPD_PATH@#${vars['MAPD_PATH']}#g" \
     -e "s#@MAPD_STORAGE@#${vars['MAPD_STORAGE']}#g" \
     -e "s#@MAPD_DATA@#${vars['MAPD_DATA']}#g" \
     -e "s#@MAPD_USER@#${vars['MAPD_USER']}#g" \
     -e "s#@MAPD_GROUP@#${vars['MAPD_GROUP']}#g" \
-    -e "s#@CUDA_HOME@#${vars['CUDA_HOME']}#g" \
     mapd.conf.in > mapd.conf
 sudo cp mapd.conf ${vars['MAPD_STORAGE']}
 
 systemctl daemon-reload
+
+if [ ! -d "${vars['MAPD_DATA']}/mapd_catalogs" ]; then
+  echo
+  echo "${vars['MAPD_DATA']}/mapd_catalogs does not exist. Please run:"
+  echo "  ${vars["MAPD_PATH"]}/bin/initdb ${vars['MAPD_DATA']}"
+fi
