84661f8c64ec56ac43ee81ce3884de90c9537da5
mimc <michaelymg@gmail.com>
2016-02-05 14:41:34 -0800

    Avoid duplicate cubin compilation for all GPUs; refine GpuCompilationContext

--- QueryEngine/NvidiaKernel.h
+++ QueryEngine/NvidiaKernel.h
@@ -3,22 +3,36 @@
 
 #include <cuda.h>
 #include <string>
+#include <vector>
+
+#ifdef HAVE_CUDA
+namespace {
+void fill_options(std::vector<CUjit_option>& option_keys,
+                  std::vector<void*>& option_values,
+                  const unsigned block_size_x) {
+  option_keys.push_back(CU_JIT_LOG_VERBOSE);
+  option_values.push_back(reinterpret_cast<void*>(1));
+  option_keys.push_back(CU_JIT_THREADS_PER_BLOCK);
+  option_values.push_back(reinterpret_cast<void*>(block_size_x));
+}
+}
+#endif
 
 class GpuCompilationContext {
  public:
-  GpuCompilationContext(const char* ptx,
-                        const std::string& func_name,
-                        const std::string& lib_path,
+  GpuCompilationContext(const void* image,
+                        const std::string& kernel_name,
                         const int device_id,
                         const void* cuda_mgr,
-                        const unsigned block_size_x);
+                        unsigned int num_options,
+                        CUjit_option* options,
+                        void** option_vals);
   ~GpuCompilationContext();
   CUfunction kernel() { return kernel_; }
 
  private:
   CUmodule module_;
   CUfunction kernel_;
-  CUlinkState link_state_;
   const int device_id_;
   const void* cuda_mgr_;
 };
