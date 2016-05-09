620b190d897b1e88f91703f25b70c76a19be23a8
Alex Șuhan <alex.suhan@gmail.com>
2016-05-09 15:32:14 -0700

    Move most of ResultRows to a separate file

--- QueryEngine/AggregateUtils.h
+++ QueryEngine/AggregateUtils.h
@@ -2,34 +2,9 @@
 #define QUERYENGINE_AGGREGATEUTILS_H
 
 #include "BufferCompaction.h"
+#include "../Shared/sqltypes.h"
 
-inline int64_t get_component(const int8_t* group_by_buffer, const size_t comp_sz, const size_t index = 0) {
-  int64_t ret = std::numeric_limits<int64_t>::min();
-  switch (comp_sz) {
-    case 1: {
-      ret = group_by_buffer[index];
-      break;
-    }
-    case 2: {
-      const int16_t* buffer_ptr = reinterpret_cast<const int16_t*>(group_by_buffer);
-      ret = buffer_ptr[index];
-      break;
-    }
-    case 4: {
-      const int32_t* buffer_ptr = reinterpret_cast<const int32_t*>(group_by_buffer);
-      ret = buffer_ptr[index];
-      break;
-    }
-    case 8: {
-      const int64_t* buffer_ptr = reinterpret_cast<const int64_t*>(group_by_buffer);
-      ret = buffer_ptr[index];
-      break;
-    }
-    default:
-      CHECK(false);
-  }
-  return ret;
-}
+#include <glog/logging.h>
 
 inline void set_component(int8_t* group_by_buffer, const size_t comp_sz, const int64_t val, const size_t index = 0) {
   switch (comp_sz) {
