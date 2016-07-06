da86a4442d29ebaa8862706ef375afc707060011
Alex Șuhan <alex.suhan@gmail.com>
2016-07-06 11:54:28 -0700

    Extension function signature parser

---
+++ java/src/main/java/com/mapd/parser/server/ExtensionFunction.java
@@ -0,0 +1,66 @@
+package com.mapd.parser.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alex
+ */
+public class ExtensionFunction {
+  public enum ExtArgumentType { Int16, Int32, Int64, Float, Double };
+
+  ExtensionFunction(final List<ExtArgumentType> args, final ExtArgumentType ret) {
+    this.args = args;
+    this.ret = ret;
+  }
+
+  List<ExtArgumentType> getArgs() {
+    return this.args;
+  }
+
+  ExtArgumentType getRet() {
+    return this.ret;
+  }
+
+  String toJson(final String name) {
+    StringBuilder json_cons = new StringBuilder();
+    json_cons.append("{");
+    json_cons.append("\"name\":").append(dq(name)).append(",");
+    json_cons.append("\"ret\":").append(dq(typeName(ret))).append(",");
+    json_cons.append("\"args\":");
+    json_cons.append("[");
+    List<String> param_list = new ArrayList<String>();
+    for (final ExtArgumentType arg : args) {
+      param_list.add(dq(typeName(arg)));
+    }
+    json_cons.append(ExtensionFunctionSignatureParser.join(param_list, ","));
+    json_cons.append("]");
+    json_cons.append("}");
+    return json_cons.toString();
+  }
+
+  private static String typeName(final ExtArgumentType type) {
+    switch (type) {
+      case Int16:
+        return "i16";
+      case Int32:
+        return "i32";
+      case Int64:
+        return "i64";
+      case Float:
+        return "float";
+      case Double:
+        return "double";
+    }
+    assert false;
+    return null;
+  }
+
+  private static String dq(final String str) {
+    return "\"" + str + "\"";
+  }
+
+  private final List<ExtArgumentType> args;
+  private final ExtArgumentType ret;
+}
---
+++ java/src/main/java/com/mapd/parser/server/ExtensionFunctionSignatureParser.java
@@ -0,0 +1,89 @@
+package com.mapd.parser.server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author alex
+ */
+class ExtensionFunctionSignatureParser {
+  static Map<String, ExtensionFunction> parse(final String file_path) throws IOException {
+    File file = new File(file_path);
+    FileReader fileReader = new FileReader(file);
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+    String line;
+    Pattern r = Pattern.compile("([\\w]+) '([\\w]+) \\((.*)\\)'");
+    Map<String, ExtensionFunction> sigs = new HashMap<String, ExtensionFunction>();
+    while ((line = bufferedReader.readLine()) != null) {
+      if (line.contains("ExtensionFunctions") && line.contains("FunctionDecl")) {
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+          final String name = m.group(1);
+          final String ret = m.group(2);
+          final String cs_param_list = m.group(3);
+          sigs.put(name, toSignature(ret, cs_param_list));
+        }
+      }
+    }
+    return sigs;
+  }
+
+  static String signaturesToJson(final Map<String, ExtensionFunction> sigs) {
+    List<String> json_sigs = new ArrayList<String>();
+    for (Map.Entry<String, ExtensionFunction> sig : sigs.entrySet()) {
+      json_sigs.add(sig.getValue().toJson(sig.getKey()));
+    }
+    return "[" + join(json_sigs, ",") + "]";
+  }
+
+  private static ExtensionFunction toSignature(final String ret, final String cs_param_list) {
+    String[] params = cs_param_list.split(", ");
+    List<ExtensionFunction.ExtArgumentType> args = new ArrayList<ExtensionFunction.ExtArgumentType>();
+    for (final String param : params) {
+      args.add(deserializeType(param));
+    }
+    return new ExtensionFunction(args, deserializeType(ret));
+  }
+
+  private static ExtensionFunction.ExtArgumentType deserializeType(final String type_name) {
+    final String const_prefix = "const ";
+    if (type_name.startsWith(const_prefix)) {
+      return deserializeType(type_name.substring(const_prefix.length()));
+    }
+    if (type_name.equals("int32_t")) {
+      return ExtensionFunction.ExtArgumentType.Int32;
+    }
+    if (type_name.equals("int64_t")) {
+      return ExtensionFunction.ExtArgumentType.Int64;
+    }
+    if (type_name.equals("float")) {
+      return ExtensionFunction.ExtArgumentType.Float;
+    }
+    if (type_name.equals("double")) {
+      return ExtensionFunction.ExtArgumentType.Double;
+    }
+    assert false;
+    return null;
+  }
+
+  static String join(final List<String> strs, final String sep) {
+    StringBuilder sb = new StringBuilder();
+    if (strs.isEmpty()) {
+      return "";
+    }
+    sb.append(strs.get(0));
+    for (int i = 1; i < strs.size(); ++i) {
+      sb.append(sep).append(strs.get(i));
+    }
+    return sb.toString();
+  }
+}
