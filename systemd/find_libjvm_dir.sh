1d683d8f958f0bf2652b6c22ef153635cdf917e2
Andrew Seidl <dev@aas.io>
2016-08-23 23:02:10 -0700

    Find symlink to jre dir instead of direct path

--- systemd/find_libjvm_dir.sh
+++ systemd/find_libjvm_dir.sh
@@ -6,19 +6,21 @@ if hash java 2>/dev/null; then
 fi
 
 JVM_DIRS="
-  $JVM_DIR
-  /usr/lib/jvm/default-java/jre/lib/amd64/server
   /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/amd64/server
   /usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/lib/amd64/server
-  /usr/lib/jvm/java/jre/lib/amd64/server
+  /usr/lib/jvm/jre-1.8.0-openjdk/lib/amd64/server
+  /usr/lib/jvm/jre-1.7.0-openjdk/lib/amd64/server
   /usr/lib/jvm/java-1.8.0-openjdk/jre/lib/amd64/server
   /usr/lib/jvm/java-1.7.0-openjdk/jre/lib/amd64/server
+  /usr/lib/jvm/java/jre/lib/amd64/server
+  /usr/lib/jvm/default-java/jre/lib/amd64/server
+  $JVM_DIR
 "
 
 
 for i in $JVM_DIRS ; do
   if [ -e "$i/libjvm.so" ]; then
-    LIBJVM_DIR=$(readlink -e "$i")
+    LIBJVM_DIR="$i"
     break
   fi
 done
