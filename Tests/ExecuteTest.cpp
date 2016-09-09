c9c1a44b820550650b2478ca7a9bfd0033aa7ab8
Alex Șuhan <alex.suhan@gmail.com>
2016-09-09 12:09:15 -0700

    Add some tests for fixed encoding dictionary strings
    More to come.

--- Tests/ExecuteTest.cpp
+++ Tests/ExecuteTest.cpp
@@ -718,6 +718,10 @@ TEST(Select, Strings) {
     c("SELECT COUNT(*) FROM test WHERE str = real_str;", dt);
     c("SELECT COUNT(*) FROM test WHERE str <> str;", dt);
     c("SELECT COUNT(*) FROM test WHERE LENGTH(str) = 3;", dt);
+    c("SELECT fixed_str, COUNT(*) FROM test GROUP BY fixed_str HAVING COUNT(*) > 5 ORDER BY fixed_str;", dt);
+    c("SELECT fixed_str, COUNT(*) FROM test WHERE fixed_str = 'bar' GROUP BY fixed_str HAVING COUNT(*) > 4 ORDER BY "
+      "fixed_str;",
+      dt);
     ASSERT_EQ(2 * g_num_rows, v<int64_t>(run_simple_agg("SELECT COUNT(*) FROM test WHERE CHAR_LENGTH(str) = 3;", dt)));
     ASSERT_EQ(g_num_rows, v<int64_t>(run_simple_agg("SELECT COUNT(*) FROM test WHERE str ILIKE 'f%%';", dt)));
     ASSERT_EQ(0, v<int64_t>(run_simple_agg("SELECT COUNT(*) FROM test WHERE str ILIKE 'McDonald''s';", dt)));
@@ -2025,13 +2029,15 @@ int main(int argc, char** argv) {
     g_sqlite_comparator.query(drop_old_test);
     const std::string create_test{
         "CREATE TABLE test(x int not null, y int, z smallint, t bigint, b boolean, f float, d double, str text "
-        "encoding dict, real_str text, m timestamp(0), n time(0), o date, fx int encoding fixed(16), dd decimal(10, "
+        "encoding dict, fixed_str text encoding dict(16), real_str text, m timestamp(0), n time(0), o date, fx int "
+        "encoding fixed(16), dd decimal(10, "
         "2), ss text encoding dict, u int, ofd int, ufd int not null, ofq bigint, ufq bigint not null) WITH "
         "(fragment_size=2);"};
     run_ddl_statement(create_test);
     g_sqlite_comparator.query(
         "CREATE TABLE test(x int not null, y int, z smallint, t bigint, b boolean, f float, d double, str text, "
-        "real_str text, m timestamp(0), n time(0), o date, fx int, dd decimal(10, 2), ss text, u int, ofd int, ufd int "
+        "fixed_str text, real_str text, m timestamp(0), n time(0), o date, fx int, dd decimal(10, 2), ss text, u int, "
+        "ofd int, ufd int "
         "not null, ofq bigint, ufq bigint not null);");
   } catch (...) {
     LOG(ERROR) << "Failed to (re-)create table 'test'";
@@ -2040,7 +2046,7 @@ int main(int argc, char** argv) {
   CHECK_EQ(g_num_rows % 2, 0);
   for (ssize_t i = 0; i < g_num_rows; ++i) {
     const std::string insert_query{
-        "INSERT INTO test VALUES(7, 42, 101, 1001, 't', 1.1, 2.2, 'foo', 'real_foo', '2014-12-13 22:23:15', "
+        "INSERT INTO test VALUES(7, 42, 101, 1001, 't', 1.1, 2.2, 'foo', 'foo', 'real_foo', '2014-12-13 22:23:15', "
         "'15:13:14', "
         "'1999-09-09', 9, 111.1, 'fish', null, 2147483647, -2147483648, null, -1);"};
     run_multiple_agg(insert_query, ExecutorDeviceType::CPU);
@@ -2048,7 +2054,7 @@ int main(int argc, char** argv) {
   }
   for (ssize_t i = 0; i < g_num_rows / 2; ++i) {
     const std::string insert_query{
-        "INSERT INTO test VALUES(8, 43, 102, 1002, 'f', 1.2, 2.4, 'bar', 'real_bar', '2014-12-13 22:23:15', "
+        "INSERT INTO test VALUES(8, 43, 102, 1002, 'f', 1.2, 2.4, 'bar', 'bar', 'real_bar', '2014-12-13 22:23:15', "
         "'15:13:14', "
         "NULL, NULL, 222.2, null, null, null, -1, 9223372036854775807, -9223372036854775808);"};
     run_multiple_agg(insert_query, ExecutorDeviceType::CPU);
@@ -2056,7 +2062,7 @@ int main(int argc, char** argv) {
   }
   for (ssize_t i = 0; i < g_num_rows / 2; ++i) {
     const std::string insert_query{
-        "INSERT INTO test VALUES(7, 43, 102, 1002, 't', 1.3, 2.6, 'baz', 'real_baz', '2014-12-13 22:23:15', "
+        "INSERT INTO test VALUES(7, 43, 102, 1002, 't', 1.3, 2.6, 'baz', 'baz', 'real_baz', '2014-12-13 22:23:15', "
         "'15:13:14', "
         "'1999-09-09', 11, 333.3, 'boat', null, 1, -1, 1, -9223372036854775808);"};
     run_multiple_agg(insert_query, ExecutorDeviceType::CPU);
