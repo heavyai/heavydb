df7ee907d006790ee44d385535d1b25ab4eb3984
Andrew Seidl <dev@aas.io>
2016-09-20 16:15:27 -0700

    import: add initial test for type detection

---
+++ Tests/ImportTest.cpp
@@ -0,0 +1,59 @@
+#include "../Import/Importer.h"
+
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace {
+std::string TypeToString(SQLTypes type) {
+  return SQLTypeInfo(type, false).get_type_name();
+}
+
+void d(const SQLTypes expected_type, const std::string& str) {
+  auto detected_type = Importer_NS::Detector::detect_sqltype(str);
+  EXPECT_EQ(TypeToString(expected_type), TypeToString(detected_type)) << "String: " << str;
+}
+
+TEST(Detect, DateTime) {
+  d(kDATE, "2016-01-02");
+  d(kDATE, "02/01/2016");
+  d(kDATE, "01-Feb-16");
+  d(kDATE, "01/Feb/2016");
+  d(kDATE, "01/Feb/16");
+  d(kTIMESTAMP, "2016-01-02T03:04");
+  d(kTIMESTAMP, "2016-01-02T030405");
+  d(kTIMESTAMP, "2016-01-02T03:04:05");
+  d(kTIMESTAMP, "1776-01-02T03:04:05");
+  d(kTIMESTAMP, "9999-01-02T03:04:05");
+  d(kTIME, "03:04");
+  d(kTIME, "03:04:05");
+  d(kTEXT, "33:04");
+}
+
+TEST(Detect, Numeric) {
+  d(kSMALLINT, "1");
+  d(kSMALLINT, "12345");
+  d(kINT, "123456");
+  d(kINT, "1234567890");
+  d(kBIGINT, "12345678901");
+  d(kFLOAT, "1.");
+  d(kFLOAT, "1.2345678");
+  // d(kDOUBLE, "1.2345678901");
+  // d(kDOUBLE, "1.23456789012345678901234567890");
+  d(kTEXT, "1.22.22");
+}
+}
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  testing::InitGoogleTest(&argc, argv);
+
+  int err{0};
+  try {
+    err = RUN_ALL_TESTS();
+  } catch (const std::exception& e) {
+    LOG(ERROR) << e.what();
+  }
+  return err;
+}
