4b5e0333acf52a084faadd2e1f6851fcdf0d1fbb
Alex Șuhan <alex.suhan@gmail.com>
2016-09-20 17:06:31 -0700

    Change dictionary ownership in ResultSet::createComparator
    We need to take ownership through the Executor until we're further along with
    the integration and we can clean up this dependency.

--- QueryEngine/ResultSet.cpp
+++ QueryEngine/ResultSet.cpp
@@ -151,7 +151,7 @@ std::function<bool(const uint32_t, const uint32_t)> ResultSet::createComparator(
         CHECK(rhs_v.isInt());
         if (UNLIKELY(is_dict)) {
           CHECK_EQ(4, entry_ti.get_logical_size());
-          auto string_dict = row_set_mem_owner_->getStringDict(entry_ti.get_comp_param());
+          const auto string_dict = executor_->getStringDictionary(entry_ti.get_comp_param(), row_set_mem_owner_);
           auto lhs_str = string_dict->getString(lhs_v.i1);
           auto rhs_str = string_dict->getString(rhs_v.i1);
           if (lhs_str == rhs_str) {
