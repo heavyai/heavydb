c1f70d38dc32aac67d2f19a137fb22e88dd0cb87
Alex Șuhan <alex.suhan@gmail.com>
2016-11-17 22:13:27 -0800

    Fix linking issues caused by LLVMGlobalContext.cpp

--- QueryEngine/LLVMGlobalContext.cpp
+++ QueryEngine/LLVMGlobalContext.cpp
@@ -1,4 +1,7 @@
 #include "LLVMGlobalContext.h"
+
+#if LLVM_VERSION_MAJOR == 3 && LLVM_VERSION_MINOR >= 9
+
 #include <llvm/Support/ManagedStatic.h>
 
 namespace {
@@ -10,3 +13,11 @@ llvm::ManagedStatic<llvm::LLVMContext> g_global_context;
 llvm::LLVMContext& getGlobalLLVMContext() {
   return *g_global_context;
 }
+
+#else
+
+llvm::LLVMContext& getGlobalLLVMContext() {
+  return llvm::getGlobalContext();
+}
+
+#endif
