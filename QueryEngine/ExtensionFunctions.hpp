ab4377174a8c5db9098242eaa2c9e945b2b05921
Alex Șuhan <alex.suhan@gmail.com>
2016-12-01 19:31:18 -0800

    Add ATAN runtime function

--- QueryEngine/ExtensionFunctions.hpp
+++ QueryEngine/ExtensionFunctions.hpp
@@ -25,6 +25,11 @@ double Asin(const double x) {
 }
 
 EXTENSION_NOINLINE
+double Atan(const double x) {
+  return atan(x);
+}
+
+EXTENSION_NOINLINE
 double Atan2(const double y, const double x) {
   return atan2(y, x);
 }
@@ -202,7 +207,6 @@ EXTENSION_NOINLINE
 double conv_4326_900913_y(const double y) {
   return 6378136.99911 * log(tan(.00872664626 * y + .785398163397));
 }
-
 
 /** @brief  Computes the distance, in meters, between two WGS-84 positions.
   *
