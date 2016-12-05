dcdb24efe29aa41ec68f9f99e71b8f5f77395278
Andrew Seidl <dev@aas.io>
2016-12-05 12:47:08 -0800

    Make version number more accessible

--- Shared/release.h
+++ Shared/release.h
@@ -1,3 +1,4 @@
+// clang-format off
 /**
  * @file		release.h
  * @author	Wei Hong <wei@map-d.com>
@@ -10,6 +11,18 @@
 
 #include <string>
 
-const std::string MapDRelease("@MAPD_VERSION_RAW@-@MAPD_BUILD_DATE@-@MAPD_GIT_HASH@");
+static const int32_t MAPD_VERSION_MAJOR{@MAPD_VERSION_MAJOR@};
+static const int32_t MAPD_VERSION_MINOR{@MAPD_VERSION_MINOR@};
+static const int32_t MAPD_VERSION_PATCH{@MAPD_VERSION_PATCH@};
+static const int32_t MAPD_VERSION{
+  @MAPD_VERSION_MAJOR@ * 1000000 + @MAPD_VERSION_MINOR@ * 1000 + @MAPD_VERSION_PATCH@
+};
+
+static const std::string MAPD_VERSION_EXTRA{"@MAPD_VERSION_EXTRA@"};
+static const std::string MAPD_VERSION_RAW{"@MAPD_VERSION_RAW@"};
+static const std::string MAPD_BUILD_DATE{"@MAPD_BUILD_DATE@"};
+static const std::string MAPD_GIT_HASH{"@MAPD_GIT_HASH@"};
+
+static const std::string MapDRelease{"@MAPD_VERSION_RAW@-@MAPD_BUILD_DATE@-@MAPD_GIT_HASH@"};
 
 #endif  // RELEASE_H
