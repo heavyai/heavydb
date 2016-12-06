1678d1ff87bde5efd28cbe2b4eb096d15f5a349b
Alex Șuhan <alex.suhan@gmail.com>
2016-12-06 11:37:53 -0800

    Allow up to 30M groups on the baseline path

--- QueryEngine/GroupByAndAggregate.cpp
+++ QueryEngine/GroupByAndAggregate.cpp
@@ -1501,7 +1501,7 @@ void GroupByAndAggregate::initQueryMemoryDescriptor(const bool allow_multifrag,
   const auto col_range_info = getColRangeInfo();
 
   if (g_enable_watchdog && col_range_info.hash_type_ == GroupByColRangeType::MultiCol &&
-      max_groups_buffer_entry_count > 20000000) {
+      max_groups_buffer_entry_count > 60000000) {
     throw WatchdogException("Query would use too much memory");
   }
 
