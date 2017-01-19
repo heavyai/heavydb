bd70126a2824a7bd418c6d3a7aaa942a05feef60
Michael Thomson <michael@mapd.com>
2017-01-18 17:18:14 -0800

    Revert "Remove the coarse high level lock everything lock in sql_execute"
    This reverts commit 3db50314eadfcadca87d1098f9c5bdbce58f9619.

--- MapDServer.cpp
+++ MapDServer.cpp
@@ -255,6 +255,7 @@ class MapDHandler : virtual public MapDIf {
         break;
       case ExecutorDeviceType::Hybrid:
         LOG(INFO) << "Started in Hybrid mode" << std::endl;
+        break;
     }
 
 
@@ -511,6 +512,11 @@ class MapDHandler : virtual public MapDIf {
                    const std::string& query_str,
                    const bool column_format,
                    const std::string& nonce) override {
+    std::unique_ptr<std::lock_guard<std::mutex>> render_lock;
+    ParserWrapper pw{query_str};
+    if (enable_rendering_ && !pw.is_ddl && !pw.is_update_dml) {
+      render_lock.reset(new std::lock_guard<std::mutex>(render_mutex_));
+    }
     const auto session_info = get_session(session);
     if (leaf_aggregator_.leafCount() > 0) {
 #ifdef HAVE_RAVM
@@ -528,7 +534,7 @@ class MapDHandler : virtual public MapDIf {
       CHECK(false);
 #endif  // HAVE_RAVM
     } else {
-      sql_execute_impl(_return, session_info, query_str, column_format, nonce, session_info.get_executor_device_type());
+      sql_execute_impl(_return, session_info, query_str, column_format, nonce);
     }
   }
 
@@ -1787,11 +1793,11 @@ class MapDHandler : virtual public MapDIf {
                         const Catalog_Namespace::SessionInfo& session_info,
                         const std::string& query_str,
                         const bool column_format,
-                        const std::string& nonce,
-                        const ExecutorDeviceType executor_device_type) {
+                        const std::string& nonce) {
     _return.nonce = nonce;
     _return.execution_time_ms = 0;
     auto& cat = session_info.get_catalog();
+    auto executor_device_type = session_info.get_executor_device_type();
     LOG(INFO) << query_str;
     _return.total_time_ms = measure<>::execution([&]() {
       SQLParser parser;
