95148265ad776e924d9be3ee54ca59e533b05e93
Andrew Seidl <dev@aas.io>
2017-04-06 13:35:29 -0700

    thrift: python: more precise recursive struct rewriter
    Previously moved around all `thrift_spec` nodes in order to work around
    the recursive struct issue. This caused problems for struct members with
    default values, where the full `thrift_spec` must exist.
    
    Fix is to only move the `thrift_spec` for the types which actually
    require the delayed init.

--- SampleCode/fix_recursive_structs.py
+++ SampleCode/fix_recursive_structs.py
@@ -5,9 +5,8 @@ import sys
 
 
 def main():
-    """
-    Rewrite Thrift-generated Python clients to handle recursive structs.
-    For more details see: https://issues.apache.org/jira/browse/THRIFT-2642
+    """Rewrite Thrift-generated Python clients to handle recursive structs. For
+    more details see: https://issues.apache.org/jira/browse/THRIFT-2642.
 
     Requires package `RedBaron`, available via pip:
     $ pip install redbaron
@@ -17,14 +16,15 @@ def main():
     $ thrift -gen py mapd.thrift
     $ mv gen-py/mapd/ttypes.py gen-py/mapd/ttypes-backup.py
     $ python fix_recursive_structs.py gen-py/mapd/ttypes-backup.py gen-py/mapd/ttypes.py
+
     """
     in_file = open(sys.argv[1], 'r')
     out_file = open(sys.argv[2], 'w')
 
     red_ast = RedBaron(in_file.read())
 
-    thrift_specs = [td.parent for td in red_ast.find_all(
-        'name', 'thrift_spec') if td.parent.type == 'assignment']
+    thrift_specs = [ts.parent for ts in red_ast.find_all(
+        'name', 'thrift_spec') if ts.parent.type == 'assignment' and ts.parent.parent.name in ['TDatumVal', 'TColumnData']]
 
     nodes = []
     for ts in thrift_specs:
@@ -35,6 +35,7 @@ def main():
 
     red_ast.extend(nodes)
     out_file.write(red_ast.dumps())
+
 
 if __name__ == '__main__':
     main()
