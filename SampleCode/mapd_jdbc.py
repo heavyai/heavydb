fbeddea45286a302b97c3de67bdedee8d88d3f67
Ralph Loen <ralph@mapd.com>
2017-05-15 13:40:51 -0700

    sample: fix typo in JDBC connection string
    Issue introduced by addition of `http` flag in JDBC driver.
    
    See: https://community.mapd.com/t/python-jdbc-wrapper/66

--- SampleCode/mapd_jdbc.py
+++ SampleCode/mapd_jdbc.py
@@ -7,7 +7,7 @@ def connect(dbname, user, host, password):
     jar = './mapdjdbc-1.0-SNAPSHOT-jar-with-dependencies.jar'  # may want to parametrize
     try:
         return jaydebeapi.connect('com.mapd.jdbc.MapDDriver',
-                                  'jdbc:mapd:{}:{}:.'.format(host, dbname),
+                                  'jdbc:mapd:{}:{}:'.format(host, dbname),
                                   {'user': user, 'password': password},
                                   jar)
     except Exception as e:
