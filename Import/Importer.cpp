234f200139b3d9795c51df794ec59b312253517c
Alex Şuhan <alex.suhan@gmail.com>
2017-07-05 13:37:06 -0700

    Make loader sharding aware

--- Import/Importer.cpp
+++ Import/Importer.cpp
@@ -40,8 +40,10 @@
 #include <ogrsf_frmts.h>
 #include <gdal.h>
 #include "../QueryEngine/SqlTypesLayout.h"
+#include "../QueryEngine/TypePunning.h"
 #include "../Shared/mapdpath.h"
 #include "../Shared/measure.h"
+#include "../Shared/unreachable.h"
 #include "../Shared/geosupport.h"
 #include "Importer.h"
 #include "gen-cpp/MapD.h"
@@ -723,9 +725,168 @@ bool Loader::load(const std::vector<std::unique_ptr<TypedImportBuffer>>& import_
   return loadImpl(import_buffers, row_count, true);
 }
 
+namespace {
+
+int64_t int_value_at(const TypedImportBuffer& import_buffer, const size_t index) {
+  const auto& ti = import_buffer.getTypeInfo();
+  const int8_t* values_buffer{nullptr};
+  if (ti.is_string()) {
+    CHECK_EQ(kENCODING_DICT, ti.get_compression());
+    values_buffer = import_buffer.getStringDictBuffer();
+  } else {
+    values_buffer = import_buffer.getAsBytes();
+  }
+  CHECK(values_buffer);
+  switch (ti.get_logical_size()) {
+    case 1: {
+      return values_buffer[index];
+    }
+    case 2: {
+      return reinterpret_cast<const int16_t*>(values_buffer)[index];
+    }
+    case 4: {
+      return reinterpret_cast<const int32_t*>(values_buffer)[index];
+    }
+    case 8: {
+      return reinterpret_cast<const int64_t*>(values_buffer)[index];
+    }
+    default:
+      CHECK(false);
+  }
+  UNREACHABLE();
+  return 0;
+}
+
+float float_value_at(const TypedImportBuffer& import_buffer, const size_t index) {
+  const auto& ti = import_buffer.getTypeInfo();
+  CHECK_EQ(kFLOAT, ti.get_type());
+  const auto values_buffer = import_buffer.getAsBytes();
+  return reinterpret_cast<const float*>(may_alias_ptr(values_buffer))[index];
+}
+
+double double_value_at(const TypedImportBuffer& import_buffer, const size_t index) {
+  const auto& ti = import_buffer.getTypeInfo();
+  CHECK_EQ(kDOUBLE, ti.get_type());
+  const auto values_buffer = import_buffer.getAsBytes();
+  return reinterpret_cast<const double*>(may_alias_ptr(values_buffer))[index];
+}
+
+}  // namespace
+
+void Loader::distributeToShards(std::vector<OneShardBuffers>& all_shard_import_buffers,
+                                std::vector<size_t>& all_shard_row_counts,
+                                const OneShardBuffers& import_buffers,
+                                const size_t row_count) {
+  const auto shard_tables = catalog.getPhysicalTablesDescriptors(table_desc);
+  all_shard_row_counts.resize(shard_tables.size());
+  for (size_t shard_idx = 0; shard_idx < shard_tables.size(); ++shard_idx) {
+    all_shard_import_buffers.emplace_back();
+    for (const auto& typed_import_buffer : import_buffers) {
+      all_shard_import_buffers.back().emplace_back(
+          new TypedImportBuffer(typed_import_buffer->getColumnDesc(), typed_import_buffer->getStringDictionary()));
+    }
+  }
+  CHECK_GT(table_desc->shardedColumnId, 0);
+  int col_idx{0};
+  const ColumnDescriptor* shard_col_desc{nullptr};
+  for (const auto col_desc : column_descs) {
+    ++col_idx;
+    if (col_idx == table_desc->shardedColumnId) {
+      shard_col_desc = col_desc;
+      break;
+    }
+  }
+  CHECK(shard_col_desc);
+  CHECK_LE(static_cast<size_t>(table_desc->shardedColumnId), import_buffers.size());
+  const auto& shard_column_input_buffer = import_buffers[table_desc->shardedColumnId - 1];
+  if (shard_col_desc->columnType.is_integer()) {
+    for (size_t i = 0; i < row_count; ++i) {
+      const auto val = int_value_at(*shard_column_input_buffer, i);
+      const auto shard = val % shard_tables.size();
+      auto& shard_output_buffers = all_shard_import_buffers[shard];
+      for (size_t col_idx = 0; col_idx < import_buffers.size(); ++col_idx) {
+        const auto& input_buffer = import_buffers[col_idx];
+        const auto& col_ti = input_buffer->getTypeInfo();
+        const auto type = col_ti.is_decimal() ? decimal_to_int_type(col_ti) : col_ti.get_type();
+        switch (type) {
+          case kBOOLEAN:
+            shard_output_buffers[col_idx]->addBoolean(int_value_at(*input_buffer, i));
+            break;
+          case kSMALLINT:
+            shard_output_buffers[col_idx]->addSmallint(int_value_at(*input_buffer, i));
+            break;
+          case kINT:
+            shard_output_buffers[col_idx]->addInt(int_value_at(*input_buffer, i));
+            break;
+          case kBIGINT:
+            shard_output_buffers[col_idx]->addBigint(int_value_at(*input_buffer, i));
+            break;
+          case kFLOAT:
+            shard_output_buffers[col_idx]->addFloat(float_value_at(*input_buffer, i));
+            break;
+          case kDOUBLE:
+            shard_output_buffers[col_idx]->addDouble(double_value_at(*input_buffer, i));
+            break;
+          case kTEXT:
+          case kVARCHAR:
+          case kCHAR: {
+            CHECK_LT(i, input_buffer->getStringBuffer()->size());
+            shard_output_buffers[col_idx]->addString((*input_buffer->getStringBuffer())[i]);
+            break;
+          }
+          case kTIME:
+          case kTIMESTAMP:
+          case kDATE:
+            shard_output_buffers[col_idx]->addTime(int_value_at(*input_buffer, i));
+            break;
+          case kARRAY:
+            if (IS_STRING(col_ti.get_subtype())) {
+              CHECK(input_buffer->getStringArrayBuffer());
+              CHECK_LT(i, input_buffer->getStringArrayBuffer()->size());
+              const auto& input_arr = (*(input_buffer->getStringArrayBuffer()))[i];
+              shard_output_buffers[col_idx]->addStringArray(input_arr);
+            } else {
+              shard_output_buffers[col_idx]->addArray((*input_buffer->getArrayBuffer())[i]);
+            }
+            break;
+          default:
+            CHECK(false);
+        }
+      }
+      ++all_shard_row_counts[shard];
+    }
+  } else {
+    CHECK(false);
+  }
+}
+
 bool Loader::loadImpl(const std::vector<std::unique_ptr<TypedImportBuffer>>& import_buffers,
                       size_t row_count,
                       bool checkpoint) {
+  if (table_desc->nShards) {
+    std::vector<OneShardBuffers> all_shard_import_buffers;
+    std::vector<size_t> all_shard_row_counts;
+    distributeToShards(all_shard_import_buffers, all_shard_row_counts, import_buffers, row_count);
+    const auto shard_tables = catalog.getPhysicalTablesDescriptors(table_desc);
+    bool success = true;
+    for (size_t shard_idx = 0; shard_idx < shard_tables.size(); ++shard_idx) {
+      if (!all_shard_row_counts[shard_idx]) {
+        continue;
+      }
+      success = success && loadToShard(all_shard_import_buffers[shard_idx],
+                                       all_shard_row_counts[shard_idx],
+                                       shard_tables[shard_idx],
+                                       checkpoint);
+    }
+    return success;
+  }
+  return loadToShard(import_buffers, row_count, table_desc, checkpoint);
+}
+
+bool Loader::loadToShard(const std::vector<std::unique_ptr<TypedImportBuffer>>& import_buffers,
+                         size_t row_count,
+                         const TableDescriptor* shard_table,
+                         bool checkpoint) {
   Fragmenter_Namespace::InsertData ins_data(insert_data);
   ins_data.numRows = row_count;
   bool success = true;
@@ -757,9 +918,9 @@ bool Loader::loadImpl(const std::vector<std::unique_ptr<TypedImportBuffer>>& imp
   {
     try {
       if (checkpoint)
-        table_desc->fragmenter->insertData(ins_data);
+        shard_table->fragmenter->insertData(ins_data);
       else
-        table_desc->fragmenter->insertDataNoCheckpoint(ins_data);
+        shard_table->fragmenter->insertDataNoCheckpoint(ins_data);
     } catch (std::exception& e) {
       LOG(ERROR) << "Fragmenter Insert Exception: " << e.what();
       success = false;
@@ -1162,8 +1323,10 @@ ImportStatus Importer::importDelimited() {
       break;
     }
     // checkpoint before going again
-    loader->get_catalog().get_dataMgr().checkpoint(loader->get_catalog().get_currentDB().dbId,
-                                                   loader->get_table_desc()->tableId);
+    const auto shard_tables = loader->get_catalog().getPhysicalTablesDescriptors(loader->get_table_desc());
+    for (const auto shard_table : shard_tables) {
+      loader->get_catalog().get_dataMgr().checkpoint(loader->get_catalog().get_currentDB().dbId, shard_table->tableId);
+    }
   }
   // todo MAT we need to review whether this checkpoint process makes sense
   auto ms = measure<>::execution([&]() {
