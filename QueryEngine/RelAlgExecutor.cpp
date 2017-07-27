0ec45f3af8d2065d08fafed85f3f30cc2e9c2ef3
Alex Şuhan <alex.suhan@gmail.com>
2017-07-27 11:20:09 -0700

    Fix approximate count distinct substitution
    The patch which improved the performance under contention by using
    multiple bitmap buffers didn't adjust the logic which replaces the
    approximate implementation with the precise one when advantageous.

--- QueryEngine/RelAlgExecutor.cpp
+++ QueryEngine/RelAlgExecutor.cpp
@@ -1231,10 +1231,10 @@ RelAlgExecutionUnit decide_approx_count_distinct_implementation(
     // must be consistent across all leaves, otherwise we could have a mix of precise
     // and approximate bitmaps and we cannot aggregate them.
     const auto device_type = g_cluster ? ExecutorDeviceType::GPU : device_type_in;
-    CountDistinctDescriptor approx_count_distinct_desc{
-        CountDistinctImplType::Bitmap, arg_range.getIntMin(), HLL_MASK_WIDTH, true, device_type, 1};
     const auto bitmap_sz_bits = arg_range.getIntMax() - arg_range.getIntMin() + 1;
     const auto sub_bitmap_count = get_count_distinct_sub_bitmap_count(bitmap_sz_bits, ra_exe_unit, device_type);
+    CountDistinctDescriptor approx_count_distinct_desc{
+        CountDistinctImplType::Bitmap, arg_range.getIntMin(), HLL_MASK_WIDTH, true, device_type, sub_bitmap_count};
     CountDistinctDescriptor precise_count_distinct_desc{
         CountDistinctImplType::Bitmap, arg_range.getIntMin(), bitmap_sz_bits, false, device_type, sub_bitmap_count};
     if (approx_count_distinct_desc.bitmapPaddedSizeBytes() >= precise_count_distinct_desc.bitmapPaddedSizeBytes()) {
