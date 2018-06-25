4090592c8302729215d07732935477e64b3535b9
Drew Ritter <arittr@gmail.com>
2018-06-25 14:48:12 -0700

    deps: macos: sync thrift.rb with upstream, 0.11.0
    Previous version used some syntax that is being deprecated in newer
    versions of Homebrew.

--- ThirdParty/Thrift/thrift.rb
+++ ThirdParty/Thrift/thrift.rb
@@ -1,11 +1,18 @@
 class Thrift < Formula
   desc "Framework for scalable cross-language services development"
   homepage "https://thrift.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.10.0/thrift-0.10.0.tar.gz"
-  sha256 "2289d02de6e8db04cbbabb921aeb62bfe3098c4c83f36eec6c31194301efa10b"
+  url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.11.0/thrift-0.11.0.tar.gz"
+  sha256 "c4ad38b6cb4a3498310d405a91fef37b9a8e79a50cd0968148ee2524d2fa60c2"
+
+  bottle do
+    cellar :any
+    sha256 "d1c648d84f21b567f1468625523b78d496d49954a3f5f28ce127f3eca7c0e2e4" => :high_sierra
+    sha256 "710f79cf150713e4e24ce03b605fcd3ea56651b58bb7afe64d8b4a948842616f" => :sierra
+    sha256 "e6f40c95f93331dda62d7cbfe0ce4f467c17e73e4a4a05f859e29a58533b52d8" => :el_capitan
+  end
 
   head do
-    url "https://git-wip-us.apache.org/repos/asf/thrift.git"
+    url "https://github.com/apache/thrift.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -20,18 +27,25 @@ class Thrift < Formula
   option "with-php", "Install PHP binding"
   option "with-libevent", "Install nonblocking server libraries"
 
+  deprecated_option "with-python" => "with-python@2"
+
   depends_on "bison" => :build
   depends_on "boost"
   depends_on "openssl"
   depends_on "libevent" => :optional
-  depends_on :python => :optional
+  depends_on "python@2" => :optional
+
+  if build.with? "java"
+    depends_on "ant" => :build
+    depends_on :java => "1.8"
+  end
 
   def install
     system "./bootstrap.sh" unless build.stable?
 
     exclusions = ["--without-ruby", "--disable-tests", "--without-php_extension"]
 
-    exclusions << "--without-python" if build.without? "python"
+    exclusions << "--without-python" if build.without? "python@2"
     exclusions << "--without-haskell" if build.without? "haskell"
     exclusions << "--without-java" if build.without? "java"
     exclusions << "--without-perl" if build.without? "perl"
@@ -43,6 +57,7 @@ class Thrift < Formula
     # Don't install extensions to /usr:
     ENV["PY_PREFIX"] = prefix
     ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
 
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
@@ -52,14 +67,19 @@ class Thrift < Formula
     ENV.deparallelize
     system "make"
     system "make", "install"
+
+    # Even when given a prefix to use it creates /usr/local/lib inside
+    # that dir & places the jars there, so we need to work around that.
+    (pkgshare/"java").install Dir["usr/local/lib/*.jar"] if build.with? "java"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     To install Ruby binding:
       gem install thrift
-
-    To install PHP extension for e.g. PHP 5.5:
-      brew install homebrew/php/php55-thrift
   EOS
+  end
+
+  test do
+    system "#{bin}/thrift", "--version"
   end
 end
