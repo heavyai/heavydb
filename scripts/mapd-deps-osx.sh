41b3d78d3d5eb121c9a7022cc0511cd7d102b211
Andrew Seidl <dev@aas.io>
2018-07-20 18:05:48 -0700

    deps: osx: use correct thrift version, switch to gdal
    - switch to vendored thrift formula version
    
    - switch to gdal from gdal-framework cask
    
    - move cuda to after remaining dependencies

--- scripts/mapd-deps-osx.sh
+++ scripts/mapd-deps-osx.sh
@@ -22,13 +22,9 @@ brew install glog
 brew install wget
 
 #brew install thrift
-#! due to recent upgrade of thrift to 0.11.0 by homebrew
-# existing core java code fails build with 0.11.0
-# need to build thrift from the 0.10.0 source code that 
-# is found in brew cache. !! download from apache thrift
-# project is not accepted by homebrew so it doesnt work! 
+# custom thrift formula pinned to specific supported version
 brew install -s $SCRIPTS_DIR/../ThirdParty/Thrift/thrift.rb
-brew switch thrift 0.10.0
+brew switch thrift 0.11.0
 
 brew install cryptopp
 brew install llvm@4
@@ -39,14 +35,8 @@ brew install libpng
 brew install libarchive
 
 brew cask install java
-brew cask install gdal-framework
+brew install gdal
 brew install maven
-
-# install CUDA (even if you don't have an nvidia GPU - some headers req'd for compilation)
-brew tap caskroom/drivers
-brew cask install nvidia-cuda
-CUDA_ROOT=$(ls -d /Developer/NVIDIA/CUDA-* | tail -n 1)
-export PATH=$CUDA_ROOT/bin/:$PATH
 
 # compile and install bison++ (default location under /usr/local is fine)
 curl -O https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/flexpp-bisonpp/bisonpp-1.21-45.tar.gz
@@ -58,6 +48,12 @@ popd
 # install AWS core and s3 sdk
 # remove -j $(proc) to avoid "clang: error: unable to execute command: posix_spawn failed: Resource temporarily unavailable""
 install_awscpp
+
+# install CUDA
+brew tap caskroom/drivers
+brew cask install nvidia-cuda
+CUDA_ROOT=$(ls -d /Developer/NVIDIA/CUDA-* | tail -n 1)
+export PATH=$CUDA_ROOT/bin/:$PATH
 
 # Finally, add a few components of llvm to your path PATH.
 # Not adding full llvm/bin to PATH since brew's `clang` breaks CUDA
