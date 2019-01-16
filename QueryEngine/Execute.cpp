e44d801b0c2ef7ef4ad5260545466ef34d415834
Alex Baden <alex.baden@mapd.com>
2019-01-15 21:26:53 -0700

    Turn allow-cpu-retry on by default
    With automatic punt to CPU, we want to automatically attempt to run on CPU if the query fails on GPU, even if watchdog is on.

--- QueryEngine/Execute.cpp
+++ QueryEngine/Execute.cpp
@@ -63,7 +63,7 @@ bool g_enable_debug_timer{false};
 bool g_enable_watchdog{false};
 bool g_enable_dynamic_watchdog{false};
 unsigned g_dynamic_watchdog_time_limit{10000};
-bool g_allow_cpu_retry{false};
+bool g_allow_cpu_retry{true};
 bool g_null_div_by_zero{false};
 unsigned g_trivial_loop_join_threshold{1000};
 bool g_from_table_reordering{true};
