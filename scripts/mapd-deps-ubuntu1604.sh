7a140dfd6b2c3e210053b8105f523ab3ec50d2b9
Andrew Seidl <dev@aas.io>
2019-05-10 16:40:54 -0700

    deps: ubuntu: move 16.04 to gcc 6, add go, gdal
    Also starts pulling in a newer version of Go and builds gdal, so the
    versions on Ubuntu 16.04 machines are more in line with those used on
    18.04+ and CentOS.
    
    cc #340

--- scripts/mapd-deps-ubuntu1604.sh
+++ scripts/mapd-deps-ubuntu1604.sh
@@ -13,7 +13,9 @@ sudo chown -R $(id -u) $PREFIX
 
 sudo apt update
 sudo apt install -y \
+    software-properties-common \
     build-essential \
+    libtool \
     ccache \
     cmake \
     cmake-curses-gui \
@@ -59,20 +61,42 @@ sudo apt install -y \
     automake \
     bison \
     flex-old \
+    libpng-dev \
+    rsync \
+    unzip \
     jq \
+    python-dev \
     python-yaml \
-    libxmlsec1-dev
+    swig
 
-# Install gcc6
-add-apt-repository ppa:ubuntu-toolchain-r/test -y
-apt update && apt upgrade -y
-apt install g++-6 -y
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 \
-                    --slave /usr/bin/g++ g++ /usr/bin/g++-6
-update-alternatives --config gcc
+# Install gcc 6
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt update
+sudo apt install -y g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 \
+                         --slave /usr/bin/g++ g++ /usr/bin/g++-6
+sudo update-alternatives --config gcc
 
 # Needed to find xmltooling and xml_security_c
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/lib64/pkgconfig:$PKG_CONFIG_PATH
+
+# GEO STUFF
+# expat
+download_make_install https://github.com/libexpat/libexpat/releases/download/R_2_2_5/expat-2.2.5.tar.bz2
+# kml
+download ${HTTP_DEPS}/libkml-master.zip
+unzip -u libkml-master.zip
+pushd libkml-master
+./autogen.sh || true
+CXXFLAGS="-std=c++03" ./configure --with-expat-include-dir=$PREFIX/include/ --with-expat-lib-dir=$PREFIX/lib --prefix=$PREFIX --disable-java --disable-python --disable-swig
+makej
+make install
+popd
+# proj.4
+download_make_install ${HTTP_DEPS}/proj-5.2.0.tar.gz
+# gdal
+download_make_install ${HTTP_DEPS}/gdal-2.3.2.tar.xz "" "--without-geos --with-libkml=$PREFIX --with-proj=$PREFIX"
+
 
 VERS=1_67_0
 # http://downloads.sourceforge.net/project/boost/boost/${VERS//_/.}/boost_$VERS.tar.bz2
@@ -149,6 +173,16 @@ popd
 ARROW_BOOST_USE_SHARED="ON"
 install_arrow
 
+VERS=1.11
+ARCH=$(uname -m)
+ARCH=${ARCH//x86_64/amd64}
+ARCH=${ARCH//aarch64/arm64}
+# https://dl.google.com/go/go$VERS.linux-$ARCH.tar.gz
+download ${HTTP_DEPS}/go$VERS.linux-$ARCH.tar.gz
+extract go$VERS.linux-$ARCH.tar.gz
+rm -rf $PREFIX/go || true
+mv go $PREFIX
+
 VERS=3.0.2
 wget --continue https://github.com/cginternals/glbinding/archive/v$VERS.tar.gz
 tar xvf v$VERS.tar.gz
@@ -168,37 +202,11 @@ make install
 popd
 
 # OpenSAML
-
-#xml-security requires xerces >= 3.2
 VERS=3.2.2
-wget https://www-us.apache.org/dist//xerces/c/3/sources/xerces-c-$VERS.tar.gz
-tar xzvf xerces-c-$VERS.tar.gz
-pushd xerces-c-$VERS
-./configure --prefix=$PREFIX
-make -j $(nproc)
-make install
-popd
-
-download_make_install ${HTTP_DEPS}/xml-security-c-2.0.0.tar.gz "" "--without-xalan"
-
-#xmltooling and opensaml needs --with-boost
-VERS=3.0.2-nolog4shib
-wget --continue ${HTTP_DEPS}/xmltooling-$VERS.tar.gz
-tar xvf xmltooling-$VERS.tar.gz
-pushd xmltooling-$VERS
-./configure --prefix=$PREFIX --with-boost=$PREFIX
-make -j $(nproc)
-make install
-popd
-
-VERS=3.0.0-nolog4shib
-wget --continue ${HTTP_DEPS}/opensaml-$VERS.tar.gz
-tar xvf opensaml-$VERS.tar.gz
-pushd opensaml-$VERS
-./configure --prefix=$PREFIX --with-boost=$PREFIX
-make -j $(nproc)
-make install
-popd
+download_make_install ${HTTP_DEPS}/xerces-c-3.2.2.tar.gz
+download_make_install ${HTTP_DEPS}/xml-security-c-2.0.2.tar.gz "" "--without-xalan"
+download_make_install ${HTTP_DEPS}/xmltooling-3.0.4-nolog4shib.tar.gz "" "--with-boost=$PREFIX"
+download_make_install ${HTTP_DEPS}/opensaml-3.0.1-nolog4shib.tar.gz "" "--with-boost=$PREFIX"
 
 cat > $PREFIX/mapd-deps.sh <<EOF
 PREFIX=$PREFIX
@@ -208,11 +216,14 @@ LD_LIBRARY_PATH=\$PREFIX/lib:\$LD_LIBRARY_PATH
 LD_LIBRARY_PATH=\$PREFIX/lib64:\$LD_LIBRARY_PATH
 
 PATH=/usr/local/cuda/bin:\$PATH
+PATH=\$PREFIX/go/bin:\$PATH
 PATH=\$PREFIX/bin:\$PATH
 
 CMAKE_PREFIX_PATH=\$PREFIX:\$CMAKE_PREFIX_PATH
 
-export LD_LIBRARY_PATH PATH CMAKE_PREFIX_PATH
+GOROOT=\$PREFIX/go
+
+export LD_LIBRARY_PATH PATH CMAKE_PREFIX_PATH GOROOT
 EOF
 
 echo
