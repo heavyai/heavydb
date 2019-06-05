17a596ef0a4a75757b507cbc0f66c9d4500fd965
Alex Baden <alex.baden@mapd.com>
2019-06-04 21:32:00 -0600

    Remove support for compilers without shared mutex support

--- Shared/mapd_shared_mutex.h
+++ Shared/mapd_shared_mutex.h
@@ -17,28 +17,17 @@
 #ifndef MAPD_SHARED_MUTEX
 #define MAPD_SHARED_MUTEX
 
-#ifdef HAS_SHARED_MUTEX
 #include <shared_mutex>
+
 #ifdef HAVE_FOLLY
 #include <folly/SharedMutex.h>
 using mapd_shared_mutex = folly::SharedMutex;
 #else
 using mapd_shared_mutex = std::shared_timed_mutex;
 #endif  // HAVE_FOLLY
+
 #define mapd_lock_guard std::lock_guard
 #define mapd_unique_lock std::unique_lock
 #define mapd_shared_lock std::shared_lock
-#else
-#include <boost/thread/shared_mutex.hpp>
-#ifdef HAVE_FOLLY
-#include <folly/SharedMutex.h>
-using mapd_shared_mutex = folly::SharedMutex;
-#else
-using mapd_shared_mutex = boost::shared_mutex;
-#endif  // HAVE_FOLLY
-#define mapd_lock_guard boost::lock_guard
-#define mapd_unique_lock boost::unique_lock
-#define mapd_shared_lock boost::shared_lock
-#endif  // HAS_SHARED_MUTEX
 
 #endif  // MAPD_SHARED_MUTEX
