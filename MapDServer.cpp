2620b032d9ae557c0ebde764d47bf6b8f625a2e4
Christopher Root <chris@mapd.com>
2019-06-26 12:31:45 -0700

    Auto-clears renderer gpu memory on an OOM error

--- MapDServer.cpp
+++ MapDServer.cpp
@@ -215,6 +215,7 @@ class MapDProgramOptions {
   MapDParameters mapd_parameters;
   bool enable_rendering = false;
   bool enable_spirv = false;
+  bool enable_auto_clear_render_mem = false;
   bool enable_watchdog = true;
   bool enable_dynamic_watchdog = false;
   unsigned dynamic_watchdog_time_limit = 10000;
@@ -844,6 +845,7 @@ int main(int argc, char** argv) {
                                                   prog_config_opts.allow_loop_joins,
                                                   prog_config_opts.enable_rendering,
                                                   prog_config_opts.enable_spirv,
+                                                  prog_config_opts.enable_auto_clear_render_mem,
                                                   prog_config_opts.render_mem_bytes,
                                                   prog_config_opts.num_gpus,
                                                   prog_config_opts.start_gpu,
