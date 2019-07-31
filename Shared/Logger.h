c501ebc3e5d6542c41d28e9c5cab13fef16af96c
Alex Baden <alex.baden@mapd.com>
2019-07-31 11:21:16 -0600

    Revert default log dir from omnisci_log to mapd_log for consistency with other services

--- Shared/Logger.h
+++ Shared/Logger.h
@@ -121,7 +121,7 @@ class LogOptions {
 
  public:
   // Initialize to default values
-  boost::filesystem::path log_dir_{"omnisci_log"};
+  boost::filesystem::path log_dir_{"mapd_log"};
   // file_name_pattern and symlink are prepended with base_name.
   std::string file_name_pattern_{".{SEVERITY}.%Y%m%d-%H%M%S.log"};
   std::string symlink_{".{SEVERITY}.log"};
