90cb2facfa7a25fe61fa92159177e89b976c5022
Matt Delahousaye <matt.delahousaye@omnisci.com>
2019-10-09 18:18:51 -0700

    Sphinx docs - add analytics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -202,3 +202,8 @@ highlight_language = 'cpp'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# Javascript files to include
+html_js_files=[
+#                'segment_analytics.js'
+              ]
