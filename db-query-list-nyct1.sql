5ef4bac96e38b1f890fa76bee9115107f9fdacc2
Anton Malakhov <Anton.Malakhov@intel.com>
2019-10-15 19:35:17 -0500

    Implemented multiline db-query parser, improved nyctaxi table layout

--- db-query-list-nyct1.sql
+++ db-query-list-nyct1.sql
@@ -1,7 +1,61 @@
 USER admin omnisci {
 
 DROP TABLE trips;
-CREATE TABLE trips (trip_id BIGINT, vendor_id BIGINT, pickup_datetime TIMESTAMP, dropoff_datetime TIMESTAMP, store_and_fwd_flag TEXT ENCODING NONE, rate_code_id  BIGINT, pickup_longitude DOUBLE, pickup_latitude DOUBLE, dropoff_longitude DOUBLE, dropoff_latitude DOUBLE, passenger_count DOUBLE, trip_distance DOUBLE, fare_amount DOUBLE, extra DOUBLE, mta_tax DOUBLE, tip_amount DOUBLE, tolls_amount DOUBLE, ehail_fee DOUBLE, improvement_surcharge DOUBLE, total_amount DOUBLE, payment_type BIGINT, trip_type DOUBLE, pickup TEXT ENCODING NONE, dropoff TEXT ENCODING NONE, cab_type TEXT, precipitation DOUBLE, snow_depth  BIGINT, snowfall DOUBLE, max_temperature  BIGINT, min_temperature BIGINT, average_wind_speed DOUBLE, pickup_nyct2010_gid BIGINT, pickup_ctlabel BIGINT, pickup_borocode BIGINT, pickup_boroname TEXT ENCODING NONE, pickup_ct2010 BIGINT, pickup_boroct2010 BIGINT, pickup_cdeligibil TEXT ENCODING NONE, pickup_ntacode TEXT ENCODING NONE, pickup_ntaname TEXT ENCODING NONE, pickup_puma BIGINT, dropoff_nyct2010_gid BIGINT, dropoff_ctlabel BIGINT, dropoff_borocode BIGINT, dropoff_boroname TEXT ENCODING NONE, dropoff_ct2010 BIGINT, dropoff_boroct2010 BIGINT, dropoff_cdeligibil TEXT ENCODING NONE, dropoff_ntacode TEXT ENCODING NONE, dropoff_ntaname TEXT ENCODING NONE, dropoff_puma BIGINT) WITH (storage_type='CSV:trips_xaa.csv');
+
+CREATE TABLE trips (
+    trip_id BIGINT,
+    vendor_id TEXT ENCODING NONE,
+    pickup_datetime TIMESTAMP,
+    dropoff_datetime TIMESTAMP,
+    store_and_fwd_flag TEXT ENCODING NONE,
+    rate_code_id SMALLINT,
+    pickup_longitude DOUBLE,
+    pickup_latitude DOUBLE,
+    dropoff_longitude DOUBLE,
+    dropoff_latitude DOUBLE,
+    passenger_count TINYINT,
+    trip_distance DOUBLE,
+    fare_amount DOUBLE,
+    extra DOUBLE,
+    mta_tax DOUBLE,
+    tip_amount DOUBLE,
+    tolls_amount DOUBLE,
+    ehail_fee DOUBLE,
+    improvement_surcharge DOUBLE,
+    total_amount DOUBLE,
+    payment_type TEXT ENCODING NONE,
+    trip_type TINYINT,
+    pickup TEXT ENCODING NONE,
+    dropoff TEXT ENCODING NONE,
+    cab_type TEXT,
+    precipitation DOUBLE,
+    snow_depth SMALLINT,
+    snowfall DOUBLE,
+    max_temperature SMALLINT,
+    min_temperature SMALLINT,
+    average_wind_speed DOUBLE,
+    pickup_nyct2010_gid BIGINT,
+    pickup_ctlabel DOUBLE,
+    pickup_borocode INT,
+    pickup_boroname TEXT ENCODING NONE,
+    pickup_ct2010 INT,
+    pickup_boroct2010 INT,
+    pickup_cdeligibil TEXT ENCODING NONE,
+    pickup_ntacode TEXT ENCODING NONE,
+    pickup_ntaname TEXT ENCODING NONE,
+    pickup_puma INT,
+    dropoff_nyct2010_gid BIGINT,
+    dropoff_ctlabel DOUBLE,
+    dropoff_borocode BIGINT,
+    dropoff_boroname TEXT ENCODING NONE,
+    dropoff_ct2010 INT,
+    dropoff_boroct2010 INT,
+    dropoff_cdeligibil TEXT ENCODING NONE,
+    dropoff_ntacode TEXT ENCODING NONE,
+    dropoff_ntaname TEXT ENCODING NONE,
+    dropoff_puma INT
+) WITH (storage_type='CSV:trips-header.csv');
+
 
 SELECT cab_type, count(*) FROM trips GROUP BY cab_type;
 SELECT cab_type, count(*) FROM trips GROUP BY cab_type;
