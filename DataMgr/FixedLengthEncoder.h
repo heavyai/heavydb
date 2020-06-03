eb5f8c38e17ebe13db3d716769e6cbd2cd85fa2f
Artem Alekseev <artem.alekseev@intel.com>
2020-06-03 06:24:16 -0500

    Add decimal/numeric support in FSI
    Co-authored-by: Alexey Prutskov <alexey.prutskov@intel.com>

--- DataMgr/FixedLengthEncoder.h
+++ DataMgr/FixedLengthEncoder.h
@@ -25,6 +25,9 @@
 #include "Encoder.h"
 
 #include <Shared/DatumFetchers.h>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
+#include <tuple>
 
 template <typename T, typename V>
 class FixedLengthEncoder : public Encoder {
@@ -117,8 +120,32 @@ class FixedLengthEncoder : public Encoder {
     }
   }
 
-  void updateStats(const int8_t* const dst, const size_t numBytes) override {
-    CHECK(false);
+  void updateStats(const int8_t* const dst, const size_t numElements) override {
+    const V* data = reinterpret_cast<const V*>(dst);
+
+    std::tie(dataMin, dataMax, has_nulls) = tbb::parallel_reduce(
+        tbb::blocked_range(0UL, numElements),
+        std::tuple(static_cast<V>(dataMin), static_cast<V>(dataMax), has_nulls),
+        [&](const auto& range, auto init) {
+          auto [min, max, nulls] = init;
+          for (size_t i = range.begin(); i < range.end(); i++) {
+            if (data[i] != std::numeric_limits<V>::min()) {
+              decimal_overflow_validator_.validate(data[i]);
+              min = std::min(min, data[i]);
+              max = std::max(max, data[i]);
+            } else {
+              nulls = true;
+            }
+          }
+          return std::tuple(min, max, nulls);
+        },
+        [&](auto lhs, auto rhs) {
+          const auto [lhs_min, lhs_max, lhs_nulls] = lhs;
+          const auto [rhs_min, rhs_max, rhs_nulls] = rhs;
+          return std::tuple(std::min(lhs_min, rhs_min),
+                            std::max(lhs_max, rhs_max),
+                            lhs_nulls || rhs_nulls);
+        });
   }
 
   // Only called from the executor for synthesized meta-information.
