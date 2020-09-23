3d37e13362d8372c77282fa3fa1d0ee5c1a99e8d
Alex Baden <alex.baden@omnisci.com>
2020-09-22 21:21:55 -0600

    Fixup python AST parse in Windows

--- QueryEngine/scripts/parse_ast.py
+++ QueryEngine/scripts/parse_ast.py
@@ -3,7 +3,14 @@ from __future__ import print_function
 import sys
 import re
 
-for line in sys.stdin:
-    if 'FunctionDecl' in line and 'ExtensionFunctions' in line:
-        line = re.sub("-FunctionDecl.*line:[0-9]+:[0-9]+", "", line)
-        print(line, end ='')
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            if 'FunctionDecl' in line and 'ExtensionFunctions' in line:
+                line = re.sub("-FunctionDecl.*line:[0-9]+:[0-9]+", "", line)
+                print(line)
+else:
+    for line in sys.stdin:
+        if 'FunctionDecl' in line and 'ExtensionFunctions' in line:
+            line = re.sub("-FunctionDecl.*line:[0-9]+:[0-9]+", "", line)
+            print(line, end ='')
