6c2be7bbeb90293620921650deebd0bf61ef0e50
Pearu Peterson <pearu.peterson@gmail.com>
2021-01-13 20:31:12 +0200

    Drop Python 3.6 support.

--- python/setup.py
+++ python/setup.py
@@ -49,7 +49,7 @@ setup(
     author="OmniSci",
     author_email="community@omnisci.com",
     license="Apache Software License",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -57,7 +57,6 @@ setup(
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
