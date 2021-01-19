0a9c97efa4aa63f25bf0872e2df3d1edc6f08721
ienkovich <ilya.enkovich@intel.com>
2021-01-19 08:32:56 -0600

    Implement glob for Windows.
    Signed-off-by: Alex Baden <alex.baden@mapd.com>

--- OSDependent/Windows/omnisci_glob.cpp
+++ OSDependent/Windows/omnisci_glob.cpp
@@ -16,13 +16,73 @@
 
 #include "OSDependent/omnisci_glob.h"
 
+#include <boost/filesystem.hpp>
+
+#include <windows.h>
+
 #include <string>
 #include <vector>
 
+namespace fs = boost::filesystem;
+
+namespace {
+
+bool has_wildcard(const std::string& name) {
+  if (name.find('*') != std::string::npos) {
+    return true;
+  }
+  if (name.find('?') != std::string::npos) {
+    return true;
+  }
+  return false;
+}
+
+void glob(const fs::path& base, const fs::path& pattern, std::vector<std::string>& out) {
+  if (pattern.empty()) {
+    out.push_back(base.string());
+    return;
+  }
+
+  auto it = pattern.begin();
+  auto next_part = *(it++);
+  fs::path next_pattern;
+  for (; it != pattern.end(); ++it) {
+    next_pattern /= *it;
+  }
+
+  if (has_wildcard(next_part.string())) {
+    WIN32_FIND_DATA file_data;
+    auto search = base / next_part;
+#ifdef _UNICODE
+    auto handle = FindFirstFile(search.wstring().data(), &file_data);
+#else
+    auto handle = FindFirstFile(search.string().data(), &file_data);
+#endif
+    if (handle != INVALID_HANDLE_VALUE) {
+      do {
+        fs::path found_part(file_data.cFileName);
+        if (!found_part.filename_is_dot() && !found_part.filename_is_dot_dot()) {
+          glob(base / found_part, next_pattern, out);
+        }
+      } while (FindNextFile(handle, &file_data) != 0);
+      FindClose(handle);
+    }
+  } else {
+    glob(base / next_part, next_pattern, out);
+  }
+}
+
+}  // namespace
+
 namespace omnisci {
+
 std::vector<std::string> glob(const std::string& pattern) {
   std::vector<std::string> results;
-
+  fs::path pattern_path(pattern);
+  if (!pattern_path.empty()) {
+    ::glob(pattern_path.root_path(), pattern_path.relative_path(), results);
+  }
   return results;
 }
+
 }  // namespace omnisci
