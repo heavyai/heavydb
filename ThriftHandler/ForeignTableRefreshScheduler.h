700b0f59cf3d17487b8c010765901b5493cd31ff
Matt Gara <570356+mattgara@users.noreply.github.com>
2021-04-01 15:56:05 -0700

    Ensure scheduled foreign table refreshes invalidate QueryEngine caches
    * Refactor ForeignTableRefreshScheduler out of DataMgr into
    ThriftHandler to introduce dependency on QueryEngine for access to
    executor locking and cache invalidation.
    
    * Include automated tests verifying the fix in ForeignTableDmlTest but
    disabled/skipped until issues with the scheduled refresh tests are
    resolved.
    
    (cherry picked from commit 2b9c456a62c21e468aa6e4fb2837ebf0f9a3d473)

---
+++ ThriftHandler/ForeignTableRefreshScheduler.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 OmniSci, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <string>
+#include <thread>
+
+namespace foreign_storage {
+class ForeignTableRefreshScheduler {
+ public:
+  static void start(std::atomic<bool>& is_program_running);
+  static void stop();
+
+  // The following methods are for testing purposes only
+  static void setWaitDuration(int64_t duration_in_seconds);
+  static bool isRunning();
+  static bool hasRefreshedTable();
+  static void resetHasRefreshedTable();
+
+ private:
+  static void invalidateQueryEngineCaches();
+  static std::atomic<bool> is_scheduler_running_;
+  static std::chrono::seconds thread_wait_duration_;
+  static std::thread scheduler_thread_;
+  static std::atomic<bool> has_refreshed_table_;
+  static std::mutex wait_mutex_;
+  static std::condition_variable wait_condition_;
+};
+}  // namespace foreign_storage
