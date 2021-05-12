d5f5189f894fc3ccff5a268217ff24c80520d826
Artem Alekseev <artem.alekseev@intel.com>
2021-05-12 15:45:51 +0300

    Add initial level zero manager implementation

---
+++ L0Mgr/L0Mgr.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 OmniSci, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <level_zero/ze_api.h>
+
+namespace l0 {
+class L0Device {
+ private:
+  ze_context_handle_t context_;
+  ze_device_handle_t device_;
+  ze_command_queue_handle_t command_queue_;
+
+ public:
+  L0Device(ze_driver_handle_t driver, ze_device_handle_t device);
+
+  ze_context_handle_t ctx() const;
+  ze_device_handle_t device() const;
+  ze_command_queue_handle_t command_queue() const;
+
+  ze_command_list_handle_t create_command_list() const;
+
+  ~L0Device();
+};
+
+void copy_host_to_device(int8_t* device_ptr,
+                         const int8_t* host_ptr,
+                         const size_t num_bytes,
+                         ze_command_list_handle_t command_list);
+
+void copy_device_to_host(int8_t* host_ptr,
+                         const int8_t* device_ptr,
+                         const size_t num_bytes,
+                         ze_command_list_handle_t command_list);
+
+int8_t* allocate_device_mem(const size_t num_bytes,
+                            ze_command_list_handle_t command_list);
+
+std::vector<std::shared_ptr<L0Device>> get_devices();
+}  // namespace l0
