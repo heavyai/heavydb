378f186db2b5f1c5ac5b29832cb7e565fcea45e5
Alex Baden <alex.baden@mapd.com>
2021-08-09 15:09:33 -0600

    Enable two parallel executors by default

--- Shared/SystemParameters.h
+++ Shared/SystemParameters.h
@@ -70,7 +70,7 @@ struct SystemParameters {
       true;  // allow calcite to optimize the relalgebra for a view query
   size_t calcite_timeout = 5000;     // calcite connect/send/receive timeout
   size_t calcite_keepalive = false;  // calcite keepalive connection
-  int num_executors = 1;
+  int num_executors = 2;
   int num_sessions = -1;  // maximum number of user sessions
 
   SystemParameters() : cuda_block_size(0), cuda_grid_size(0), calcite_max_mem(1024) {}
