e958eea7409f3cc5815a4bb147b003d4536f693c
Alex Baden <alexbaden@users.noreply.github.com>
2021-09-20 08:58:46 -0600

    Add support for date32 in ArrowCSVForeignStorage
    * support date32
    
    * clang-format
    
    Signed-off-by: Maria Rubtsova <maria.rubtsova@intel.com>
    Signed-off-by: Alex Baden <alex.baden@mapd.com>
    
    Co-authored-by: Maria Rubtsova <maria.rubtsova@intel.com>

--- DataMgr/ForeignStorage/ArrowForeignStorage.cpp
+++ DataMgr/ForeignStorage/ArrowForeignStorage.cpp
@@ -775,6 +775,9 @@ static SQLTypeInfo getOmnisciType(const arrow::DataType& type) {
       return SQLTypeInfo(kBOOLEAN, false);
     case Type::FLOAT:
       return SQLTypeInfo(kFLOAT, false);
+    case Type::DATE32:
+    case Type::DATE64:
+      return SQLTypeInfo(kDATE, false);
     case Type::DOUBLE:
       return SQLTypeInfo(kDOUBLE, false);
       // uncomment when arrow 2.0 will be released and modin support for dictionary types
@@ -921,11 +924,12 @@ static std::shared_ptr<arrow::DataType> getArrowImportType(const SQLTypeInfo typ
       return decimal(type.get_precision(), type.get_scale());
     case kTIME:
       return time32(TimeUnit::SECOND);
-    // case kDATE:
-    // TODO(wamsi) : Remove date64() once date32() support is added in cuDF. date32()
-    // Currently support for date32() is missing in cuDF.Hence, if client requests for
-    // date on GPU, return date64() for the time being, till support is added.
-    // return device_type_ == ExecutorDeviceType::GPU ? date64() : date32();
+    case kDATE:
+#ifdef HAVE_CUDA
+      return arrow::date64();
+#else
+      return arrow::date32();
+#endif
     case kTIMESTAMP:
       switch (type.get_precision()) {
         case 0:
