615a5bcde7b00e401dac50ee7d491976fda17aa9
Steve Blackmon <steve.blackmon@omnisci.com>
2022-01-14 12:46:45 -0800

    Patch LLVM 9 to remove cyclades
    Remove sanitizer references to cyclades which has been removed from the
    Linux kernel. See https://reviews.llvm.org/D102059
    
    This incorporates the pre-existing glibc-2.31-708430 patch which
    overlaps the cyclades removal
    
    Signed-off-by: Alex Baden <alex.baden@gmail.com>

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -190,7 +190,7 @@ function install_llvm() {
     # Patch llvm 9 for glibc 2.31+ support
     # from: https://bugs.gentoo.org/708430
     pushd llvm-$VERS.src/projects/
-    sudo patch -p0 < $SCRIPTS_DIR/llvm-9-glibc-2.31-708430.patch
+    sudo patch -p0 < $SCRIPTS_DIR/llvm-9-glibc-2.31-708430-remove-cyclades.patch
     popd
 
     rm -rf build.llvm-$VERS
