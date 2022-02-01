6e12526edc04aaee61b645d9b36f3adea1e8a8ac
Todd Mostak <tmostak@gmail.com>
2022-02-01 03:31:54 -0800

    Change fatal error when encountering an untyped null projected output to a recoverable exception

--- Shared/SqlTypesLayout.h
+++ Shared/SqlTypesLayout.h
@@ -168,8 +168,9 @@ inline size_t get_bit_width(const SQLTypeInfo& ti) {
   const auto int_type = ti.is_decimal() ? kBIGINT : ti.get_type();
   switch (int_type) {
     case kNULLT:
-      LOG(FATAL) << "Untyped NULL values are not supported. Please CAST any NULL "
-                    "constants to a type.";
+      throw std::runtime_error(
+          "Untyped NULL values are not supported. Please CAST any NULL "
+          "constants to a type.");
     case kBOOLEAN:
       return 8;
     case kTINYINT:
