539887da4b68b5e884da005cd74ce9ab7327ff85
Todd Mostak <tmostak@gmail.com>
2022-02-18 05:02:02 -0800

    Fix incorrect use of pre-cache lookup value for hash table layout in device memory allocation

--- QueryEngine/JoinHashTable/BaselineJoinHashTable.cpp
+++ QueryEngine/JoinHashTable/BaselineJoinHashTable.cpp
@@ -718,7 +718,7 @@ int BaselineJoinHashTable::initHashTableForDevice(
 #ifdef HAVE_CUDA
       BaselineJoinHashTableBuilder builder;
 
-      builder.allocateDeviceMemory(layout,
+      builder.allocateDeviceMemory(hashtable_layout,
                                    getKeyComponentWidth(),
                                    getKeyComponentCount(),
                                    entry_count,
