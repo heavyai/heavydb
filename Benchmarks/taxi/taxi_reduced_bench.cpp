7db5446f8bc21017604a6291f2063cdff63373fa
Petr Kurapov <petr.kurapov@gmail.com>
2022-04-20 18:52:14 +0300

    Fix data importing and add q2,3,4 benchmarks

--- Benchmarks/taxi/taxi_reduced_bench.cpp
+++ Benchmarks/taxi/taxi_reduced_bench.cpp
@@ -8,29 +8,32 @@
 extern bool g_enable_heterogeneous_execution;
 extern bool g_enable_multifrag_heterogeneous_execution;
 boost::filesystem::path g_data_path;
+size_t g_fragment_size = 1'000'000;
 
 using namespace TestHelpers::ArrowSQLRunner;
 
 static void createTaxiReducedTable() {
-  ArrowStorage::TableOptions TO{1'000'000};
+  getStorage()->dropTable("trips");
+  ArrowStorage::TableOptions to{g_fragment_size};
   createTable("trips",
               {{"pickup_datetime", SQLTypeInfo(kTIMESTAMP, 0, 0)},
                {"passenger_count", SQLTypeInfo(kSMALLINT)},
                {"trip_distance", SQLTypeInfo(kDECIMAL, 14, 2)},
                {"total_amount", SQLTypeInfo(kDECIMAL, 14, 2)},
                {"cab_type", SQLTypeInfo(kVARCHAR, true, kENCODING_DICT)}},
-              TO);
+              to);
 }
 static void populateTaxiReducedTable() {
   namespace fs = boost::filesystem;
+  ArrowStorage::CsvParseOptions po;
+  po.header = false;
   if (fs::is_directory(g_data_path)) {
-    throw std::runtime_error("Directories are not supported.");
-  } else {
-    std::ifstream is(g_data_path);
-    std::string line;
-    while (std::getline(is, line)) {
-      insertCsvValues("trips", line);
+    for (auto it = fs::directory_iterator{g_data_path}; it != fs::directory_iterator{};
+         it++) {
+      getStorage()->appendCsvFile(it->path().string(), "trips", po);
     }
+  } else {
+    getStorage()->appendCsvFile(g_data_path.string(), "trips", po);
   }
 }
 
@@ -41,17 +44,51 @@ T v(const TargetValue& r) {
   return *p;
 }
 
-static void taxi_q1(benchmark::State& state) {
+static void warmup() {
   auto res =
       v<int64_t>(run_simple_agg("select count(*) from trips", ExecutorDeviceType::CPU));
-  std::cerr << "number of loaded tuples: " << res << std::endl;
+  std::cout << "Number of loaded tuples: " << res << std::endl;
+}
+
+static void taxi_q1(benchmark::State& state) {
   for (auto _ : state) {
     run_multiple_agg("select cab_type, count(*) from trips group by cab_type",
                      ExecutorDeviceType::CPU);
   }
 }
 
+static void taxi_q2(benchmark::State& state) {
+  for (auto _ : state) {
+    run_multiple_agg(
+        "SELECT passenger_count, avg(total_amount) FROM trips GROUP BY passenger_count",
+        ExecutorDeviceType::CPU);
+  }
+}
+
+static void taxi_q3(benchmark::State& state) {
+  for (auto _ : state) {
+    run_multiple_agg(
+        "SELECT passenger_count, extract(year from pickup_datetime) AS pickup_year, "
+        "count(*) FROM trips GROUP BY passenger_count, pickup_year",
+        ExecutorDeviceType::CPU);
+  }
+}
+
+static void taxi_q4(benchmark::State& state) {
+  for (auto _ : state) {
+    run_multiple_agg(
+        "SELECT passenger_count, extract(year from pickup_datetime) AS pickup_year, "
+        "cast(trip_distance as int) AS distance, count(*) AS the_count FROM trips GROUP "
+        "BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count "
+        "desc",
+        ExecutorDeviceType::CPU);
+  }
+}
+
 BENCHMARK(taxi_q1);
+BENCHMARK(taxi_q2);
+BENCHMARK(taxi_q3);
+BENCHMARK(taxi_q4);
 
 int main(int argc, char* argv[]) {
   ::benchmark::Initialize(&argc, argv);
@@ -71,6 +108,9 @@ int main(int argc, char* argv[]) {
                          ->implicit_value(true),
                      "Allow multifrag heterogeneous execution.");
   desc.add_options()("data", po::value<fs::path>(&g_data_path), "Path to taxi dataset.");
+  desc.add_options()("fragment-size",
+                     po::value<size_t>(&g_fragment_size)->default_value(g_fragment_size),
+                     "Table fragment size.");
 
   logger::LogOptions log_options(argv[0]);
   log_options.severity_ = logger::Severity::FATAL;
@@ -91,6 +131,7 @@ int main(int argc, char* argv[]) {
   try {
     createTaxiReducedTable();
     populateTaxiReducedTable();
+    warmup();
     ::benchmark::RunSpecifiedBenchmarks();
   } catch (const std::exception& e) {
     LOG(ERROR) << e.what();
