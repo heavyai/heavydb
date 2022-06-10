cfb8a1cb162b1856a401f92bea36ed59f1e97a90
Todd Mostak <tmostak@gmail.com>
2022-06-10 03:02:07 -0700

    Fix PointCloudLoader: move DataBufferCache and DataCache classes to separate header file, separate PDALLoader and PointCloudTableFunctions into separate header/implementation files
    Signed-off-by: jack <jack@heavy.ai>

--- QueryEngine/TableFunctions/SystemFunctions/os/Shared/TableFunctionsCommon.hpp
+++ QueryEngine/TableFunctions/SystemFunctions/os/Shared/TableFunctionsCommon.hpp
@@ -82,61 +82,6 @@ inline std::pair<int64_t, int64_t> bin_to_x_y_bin_indexes(const int64_t bin,
   return std::make_pair(bin % num_x_bins, bin / num_x_bins);
 }
 
-struct CacheDataTf {
-  int8_t* data_buffer;
-  size_t num_bytes;
-
-  CacheDataTf(const size_t num_bytes) : num_bytes(num_bytes) {
-    data_buffer = new int8_t[num_bytes];
-  }
-
-  ~CacheDataTf() { delete[] data_buffer; }
-};
-
-class DataBufferCache {
- public:
-  bool isKeyCached(const std::string& key) const;
-
-  bool isKeyCachedAndSameLength(const std::string& key, const size_t num_bytes) const;
-
-  // Assumes dest_buffer is already appropriately sized
-  template <typename T>
-  void getDataForKey(const std::string& key, T* dest_buffer) const;
-
-  template <typename T>
-  const T& getDataRefForKey(const std::string& key) const;
-
-  template <typename T>
-  const T* getDataPtrForKey(const std::string& key) const;
-
-  template <typename T>
-  void putDataForKey(const std::string& key,
-                     T* const data_buffer,
-                     const size_t num_elements);
-
- private:
-  const size_t parallel_copy_min_bytes{1 << 20};
-
-  void copyData(int8_t* dest, const int8_t* source, const size_t num_bytes) const;
-
-  std::unordered_map<std::string, std::shared_ptr<CacheDataTf>> data_cache_;
-  mutable std::shared_mutex cache_mutex_;
-};
-
-template <typename T>
-class DataCache {
- public:
-  bool isKeyCached(const std::string& key) const;
-
-  std::shared_ptr<T> getDataForKey(const std::string& key) const;
-
-  void putDataForKey(const std::string& key, std::shared_ptr<T> const data);
-
- private:
-  std::unordered_map<std::string, std::shared_ptr<T>> data_cache_;
-  mutable std::shared_mutex cache_mutex_;
-};
-
 namespace FileUtilities {
 std::vector<std::filesystem::path> get_fs_paths(const std::string& file_or_directory);
 }
