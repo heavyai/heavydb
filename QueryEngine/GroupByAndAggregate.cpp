127370816fa8b41d35bc50afe069d37740710528
yoonminnam <53632385+yoonminnam@users.noreply.github.com>
2022-07-05 13:10:24 -0700

    Count distinct operator over the output column of UDTF crashes
    * Use map instead of vector to avoid crash due to the absense of the i-th target_expr
    
    * Add tests
    
    Co-authored-by: yoonminnam <yoon-min.nam@heavy.ai>
    (cherry picked from commit c2d42ee802bc99fc32775fd73f0675661c3d142c)

--- QueryEngine/GroupByAndAggregate.cpp
+++ QueryEngine/GroupByAndAggregate.cpp
@@ -673,30 +673,33 @@ CountDistinctDescriptors init_count_distinct_descriptors(
           arg_ti.is_fp() ? no_range_info
                          : get_expr_range_info(
                                ra_exe_unit, query_infos, agg_expr->get_arg(), executor);
-      const auto original_target_expr_ti =
-          ra_exe_unit.target_exprs_original_type_infos[i];
-      if (original_target_expr_ti.get_type() == kDATE &&
-          original_target_expr_ti.get_compression() == kENCODING_DATE_IN_DAYS) {
-        // manually encode the col range of date col if necessary
-        // (see conditionally_change_arg_to_int_type function in RelAlgExecutor.cpp)
-        auto is_date_value_not_encoded = [&original_target_expr_ti](int64_t date_val) {
-          if (original_target_expr_ti.get_comp_param() == 16) {
-            return date_val < INT16_MIN || date_val > INT16_MAX;
-          } else {
-            return date_val < INT32_MIN || date_val > INT32_MIN;
+      const auto it = ra_exe_unit.target_exprs_original_type_infos.find(i);
+      if (it != ra_exe_unit.target_exprs_original_type_infos.end()) {
+        const auto& original_target_expr_ti = it->second;
+        if (original_target_expr_ti.get_type() == kDATE &&
+            original_target_expr_ti.get_compression() == kENCODING_DATE_IN_DAYS) {
+          // manually encode the col range of date col if necessary
+          // (see conditionally_change_arg_to_int_type function in RelAlgExecutor.cpp)
+          auto is_date_value_not_encoded = [&original_target_expr_ti](int64_t date_val) {
+            if (original_target_expr_ti.get_comp_param() == 16) {
+              return date_val < INT16_MIN || date_val > INT16_MAX;
+            } else {
+              return date_val < INT32_MIN || date_val > INT32_MIN;
+            }
+          };
+          if (is_date_value_not_encoded(arg_range_info.min)) {
+            // chunk metadata of the date column contains decoded value
+            // so we manually encode it again here to represent its column range correctly
+            arg_range_info.min =
+                DateConverters::get_epoch_days_from_seconds(arg_range_info.min);
           }
-        };
-        if (is_date_value_not_encoded(arg_range_info.min)) {
-          // chunk metadata of the date column contains decoded value
-          // so we manually encode it again here to represent its column range correctly
-          arg_range_info.min =
-              DateConverters::get_epoch_days_from_seconds(arg_range_info.min);
-        }
-        if (is_date_value_not_encoded(arg_range_info.max)) {
-          arg_range_info.max =
-              DateConverters::get_epoch_days_from_seconds(arg_range_info.max);
+          if (is_date_value_not_encoded(arg_range_info.max)) {
+            arg_range_info.max =
+                DateConverters::get_epoch_days_from_seconds(arg_range_info.max);
+          }
         }
       }
+
       CountDistinctImplType count_distinct_impl_type{CountDistinctImplType::UnorderedSet};
       int64_t bitmap_sz_bits{0};
       if (agg_info.agg_kind == kAPPROX_COUNT_DISTINCT) {
