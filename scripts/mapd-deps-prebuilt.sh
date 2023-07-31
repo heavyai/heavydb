a2566d84efc39cf8804b9a1c4784a9fea38cbbd8
Steve Blackmon <steve.blackmon@heavy.ai>
2023-07-31 14:24:40 -0700

    Deps: Fix Ubuntu build errors when using clang (#7452)
    Change mapd-deps-ubuntu.sh and the Ubuntu section of mapd-deps-prebuilt.sh
    to move toolchain/test PPA addition, and the gcc-11 install, until after
    the bulk package installation via apt. This avoids accidental
    installation of gcc-13 as a recommended install
    
    Signed-off-by: Misiu Godfrey <misiu.godfrey@kraken.mapd.com>

--- scripts/mapd-deps-prebuilt.sh
+++ scripts/mapd-deps-prebuilt.sh
@@ -51,12 +51,6 @@ done
 if [ "$ID" == "ubuntu" ] ; then
   sudo $PACKAGER update
 
-  # required for gcc-11 on Ubuntu < 22.04
-  if [ "$VERSION_ID" == "20.04" ] || [ "$VERSION_ID" == "19.10" ] || [ "$VERSION_ID" == "19.04" ] || [ "$VERSION_ID" == "18.04" ]; then
-    DEBIAN_FRONTEND=noninteractive sudo apt install -y software-properties-common
-    DEBIAN_FRONTEND=noninteractive sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-  fi
-
   sudo $PACKAGER install \
       software-properties-common \
       build-essential \
@@ -64,8 +58,6 @@ if [ "$ID" == "ubuntu" ] ; then
       git \
       wget \
       curl \
-      gcc-11 \
-      g++-11 \
       libboost-all-dev \
       golang \
       libssl-dev \
@@ -107,6 +99,15 @@ if [ "$ID" == "ubuntu" ] ; then
       python-yaml \
       libxerces-c-dev \
       swig
+
+  # required for gcc-11 on Ubuntu < 22.04
+  if [ "$VERSION_ID" == "20.04" ] || [ "$VERSION_ID" == "19.04" ] || [ "$VERSION_ID" == "18.04" ]; then
+    DEBIAN_FRONTEND=noninteractive sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  fi
+
+  sudo $PACKAGER install \
+      gcc-11 \
+      g++-11
 
 # Set up gcc-11 as default gcc
 sudo update-alternatives \
