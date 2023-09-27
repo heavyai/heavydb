72c90bc290b79dd30240da41c103a00720f6b050
Misiu Godfrey <misiu.godfrey@kraken.mapd.com>
2023-09-27 13:43:24 -0700

    Fix build break

--- ThriftHandler/os/RenderHandler.cpp
+++ ThriftHandler/os/RenderHandler.cpp
@@ -34,7 +34,8 @@ RenderHandler::RenderHandler(DBHandler* db_handler,
                              const bool renderer_prefer_igpu,
                              const uint32_t renderer_vulkan_timeout_ms,
                              const bool renderer_use_parallel_executors,
-                             const SystemParameters system_parameters)
+                             const SystemParameters system_parameters,
+                             const bool renderer_enable_slab_allocation)
     : impl_(nullptr) {
   throw std::runtime_error(
       "Rendering is only supported in the Enterprise and Community Editions");
