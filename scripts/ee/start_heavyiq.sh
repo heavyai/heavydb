27d513bcb402e524d4d5f01b365e57212d182d9c
paul-aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2024-03-22 11:02:07 -0700

    Revert "support for invoking heavyiq service with gunicorn conf file (#7772)" (#7798)
    This reverts commit a72bd29e01494e61eb56b513c3ef8d9f7e7865ee.
    
    Signed-off-by: Misiu Godfrey <misiu.godfrey@kraken.mapd.com>

--- scripts/ee/start_heavyiq.sh
+++ scripts/ee/start_heavyiq.sh
@@ -41,7 +41,7 @@ if [[ "$HAS_PYTHON_3_10" != "false" ]]; then
   fi
   GUNICORN_LOG_FILE=${HEAVYIQ_LOG_PREFIX}_gunicorn.log
   gunicorn --preload --log-file $GUNICORN_LOG_FILE --capture-output -t 0 -w 4 -k uvicorn.workers.UvicornWorker \
-  -b :$MAPD_HEAVYIQ_PORT -c "gunicorn.conf.py" "heavyiq.api:create_app(config_path=\"$CONFIG_FILE_ABS_PATH\")" &
+  -b :$MAPD_HEAVYIQ_PORT "heavyiq.api:create_app(config_path=\"$CONFIG_FILE_ABS_PATH\")" &
   PID3=$!
   sleep 5
   if [[ $(ps -h -p $PID3) ]]; then
