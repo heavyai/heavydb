e0d4f83f6fb4007987236f01f89b863668aed1ce
Todd Mostak <tmostak@gmail.com>
2024-08-15 10:26:17 -0400

    Make baseline sort limit configurable, bump default to 10M (#7984)
    Signed-off-by: Misiu Godfrey <misiu.godfrey@kraken.mapd.com>

--- QueryEngine/ResultSet.cpp
+++ QueryEngine/ResultSet.cpp
@@ -48,7 +48,8 @@
 #include <numeric>
 
 size_t g_parallel_top_min = 100e3;
-size_t g_parallel_top_max = 20e6;  // In effect only with g_enable_watchdog.
+size_t g_parallel_top_max = 20e6;            // In effect only with g_enable_watchdog.
+size_t g_watchdog_baseline_sort_max = 10e6;  // In effect only with g_enable_watchdog.
 size_t g_streaming_topn_max = 100e3;
 constexpr int64_t uninitialized_cached_row_count{-1};
 constexpr size_t auto_parallel_row_count_threshold{20000UL};
@@ -813,13 +814,13 @@ void ResultSet::sort(const std::list<Analyzer::OrderEntry>& order_entries,
 
   CHECK(permutation_.empty());
 
-  if (top_n && g_parallel_top_min < entryCount()) {
-    if (g_enable_watchdog && g_parallel_top_max < entryCount()) {
+  if (top_n && entryCount() > g_parallel_top_min) {
+    if (g_enable_watchdog && entryCount() > g_parallel_top_max) {
       throw WatchdogException("Sorting the result would be too slow");
     }
     parallelTop(order_entries, top_n, executor);
   } else {
-    if (g_enable_watchdog && g_baseline_groupby_threshold < entryCount()) {
+    if (g_enable_watchdog && entryCount() > g_watchdog_baseline_sort_max) {
       throw WatchdogException("Sorting the result would be too slow");
     }
     permutation_.resize(query_mem_desc_.getEntryCount());
