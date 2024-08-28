f2442ae645e0440b29e31def5f623b72fde13bb8
Simon Eves <simon.eves@heavy.ai>
2024-08-28 09:55:07 -0700

    Deprecate deps support for Ubuntu 20.04 and 23.10 (#7977)
    * Disallow those versions
    * Prompt users to upgrade to 22.04 or 24.04
    
    Signed-off-by: Misiu Godfrey <misiu.godfrey@kraken.mapd.com>

--- scripts/mapd-deps-prebuilt.sh
+++ scripts/mapd-deps-prebuilt.sh
@@ -8,8 +8,9 @@ PREFIX=/usr/local/mapd-deps
 source /etc/os-release
 if [ "$ID" == "ubuntu" ] ; then
   PACKAGER="apt -y"
-  if [ "$VERSION_ID" != "24.04" ] && [ "$VERSION_ID" != "23.10" ] && [ "$VERSION_ID" != "22.04" ] && [ "$VERSION_ID" != "20.04" ]; then
-    echo "Ubuntu 24.04, 23.10, 22.04, and 20.04 are the only debian-based releases supported by this script"
+  if [ "$VERSION_ID" != "24.04" ] && [ "$VERSION_ID" != "22.04" ]; then
+    echo "Ubuntu 24.04 and 22.04 are the only Debian-based releases supported by this script"
+    echo "If you are still using 20.04 or 23.10 then you need to upgrade!"
     exit 1
   fi
 elif [ "$ID" == "centos" ] ; then
@@ -81,11 +82,6 @@ if [ "$ID" == "ubuntu" ] ; then
 
   install_required_ubuntu_packages
 
-  if [ "$VERSION_ID" == "20.04" ]; then
-    # required for gcc-11 on Ubuntu < 22.04
-    DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  fi
-
   sudo $PACKAGER install \
       gcc-11 \
       g++-11
@@ -100,7 +96,8 @@ if [ "$ID" == "ubuntu" ] ; then
   pushd $PREFIX
 
   OS=ubuntu${VERSION_ID}
-  if [ $VERSION_ID == "24.04" ] || [ $VERSION_ID == "23.10" ]; then
+  # always tag as 22.04 for now
+  if [ $VERSION_ID == "24.04" ]; then
     OS=ubuntu22.04
   fi
   TARBALL_TSAN=""
