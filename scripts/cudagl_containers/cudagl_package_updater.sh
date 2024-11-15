f467c2c3eae872c35d2963a27fc4b8137b73cda9
Steve Blackmon <steve.blackmon@heavy.ai>
2024-11-15 12:45:33 -0800

    Script cudagl docker image creation to fascilitate security updates (#8067)
    * Add package update script generator
    
    * Add script to build and retag cudagl images
    
    * Add do nothing cudagl_package_updater script
    
    * Add update-packages option to prebuilt and ubuntu scripts
    
    * Add arg to tar builder script
    
    * Add Dockerfile to run updater script
    
    * Add patch files to remove i386 packages from opengl sub-repo and patch to build.sh
    
    * Update Dockerfile.cuda base image name and run package updater
    
    * Add README.md
    
    Signed-off-by: Misiu Godfrey <misiu.godfrey@kraken.mapd.com>

---
+++ scripts/cudagl_containers/cudagl_package_updater.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This file is a do nothing stub, and intended for x.0.0 releases
+# When building patch releases this can be replaced with a generated file
+# to apply security updates
